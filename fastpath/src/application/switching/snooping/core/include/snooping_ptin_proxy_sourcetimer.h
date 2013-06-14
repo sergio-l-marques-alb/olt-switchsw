35a9c2329c1d96806349914973fbfc6ae607a0f1
marcio-d-melo <marcio-d-melo@alticelabs.com>
2013-06-14 16:38:33 +0000

    Req OLTSWITCH-97 - Criação de um novo Timer para calendarizar o envio de reposta aos Membership Query Message - Group & Source Specific Query
    svn path=/branches/igmpv3/; revision=409

---
+++ fastpath/src/application/switching/snooping/core/include/snooping_ptin_proxy_sourcetimer.h
@@ -0,0 +1,24 @@
+/*
+ * snooping_ptin_sourcetimer.h
+ *
+ *  Created on: 30 of May 2013
+ *      Author: Márcio Daniel Melo
+ */
+
+#include "ptin_xlate_api.h"
+#include "ptin_intf.h"
+#include "ptin_utils.h"
+#include "ptin_evc.h"
+#include "usmdb_snooping_api.h"
+#include "snooping_api.h"
+#include "avl_api.h"
+#include "buff_api.h"
+#include "l7apptimer_api.h"
+#include "l7handle_api.h"
+#include "snooping.h"
+
+L7_RC_t   snoop_ptin_proxy_Sourcetimer_init(void);
+L7_RC_t   snoop_ptin_proxy_Sourcetimer_deinit(void);
+L7_RC_t   snoop_ptin_proxy_Sourcetimer_start(snoopPTinProxySourcetimer_t *pTimer, L7_uint32 timeout, L7_uchar8 mgmdsQRV, snoopPTinProxySourcetimerInfoData_t* groupData);
+L7_RC_t   snoop_ptin_proxy_Sourcetimer_stop(snoopPTinProxySourcetimer_t *pTimer);
+L7_uint32 snoop_ptin_proxy_Sourcetimer_timeleft(snoopPTinProxySourcetimer_t *pTimer);
