88323f0f3179f9b8471765f6ace108e56b234a09
marcio-d-melo <marcio-d-melo@alticelabs.com>
2013-06-28 15:30:22 +0000

    Req OLTSWITCH-97 - Fixing uninitialized variable
    svn path=/trunk/; revision=529

--- src/fastpath/src/application/switching/snooping/core/snooping.c
+++ src/fastpath/src/application/switching/snooping/core/snooping.c
@@ -346,7 +346,8 @@ L7_RC_t snoopPacketHandle(L7_netBufHandle netBufHandle,
   L7_uint32        client_idx;              /* PTin added: IGMP snooping */
 
 #if SNOOP_PTIN_IGMPv3_PROXY
-//  char             debug_buf[IPV6_DISP_ADDR_LEN];
+  char             debug_buf[IPV6_DISP_ADDR_LEN];
+  debug_buf[0]=0;
 #endif
   
   SNOOP_TRACE(SNOOP_DEBUG_PROTO, L7_AF_INET, "snoopPacketHandle");
