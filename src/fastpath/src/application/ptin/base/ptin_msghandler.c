adc5eb2cbe5f19874844a402e33baca43ab971c4
joao-v-mateiro <joao-v-mateiro@alticelabs.com>
2013-10-07 13:03:28 +0000

    OLTSWITCH-198 / OLTTS-2727 Comand Show for ETH Ring Protection status is not working on CLI
    svn path=/trunk/; revision=1014

--- src/fastpath/src/application/ptin/base/ptin_msghandler.c
+++ src/fastpath/src/application/ptin/base/ptin_msghandler.c
@@ -3087,6 +3087,7 @@ int CHMessageHandler (ipc_msg *inbuffer, ipc_msg *outbuffer)
         }
 
         SETIPCACKOK(outbuffer);
+        outbuffer->infoDim = sizeof(msg_erps_status_t);
         LOG_INFO(LOG_CTX_PTIN_MSGHANDLER,
                  "Message processed: response with %d bytes", outbuffer->infoDim);
       
