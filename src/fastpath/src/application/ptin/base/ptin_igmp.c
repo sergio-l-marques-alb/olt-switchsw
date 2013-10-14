2e9740df0b37cc2aef8e11fa8b9a1c6d963b2f14
milton-r-silva <milton-r-silva@alticelabs.com>
2013-10-14 17:47:22 +0000

    Fix OLTSWITCH-201 Fastpath enters in deadlock when reading client list in SF board (for version 3.1).
    svn path=/trunk/; revision=1019

--- src/fastpath/src/application/ptin/base/ptin_igmp.c
+++ src/fastpath/src/application/ptin/base/ptin_igmp.c
@@ -1795,6 +1795,7 @@ L7_RC_t ptin_igmp_clientList_get(L7_uint16 McastEvcId, L7_in_addr_t *ipv4_channe
       /* Restore client id structure */
       if (ptin_igmp_clientId_restore(&clientList[n_clients])!=L7_SUCCESS)
       {
+        osapiSemaGive(ptin_igmp_clients_sem);
         LOG_ERR(LOG_CTX_PTIN_IGMP,"Error restoring client id structure");
         return L7_FAILURE;
       }
