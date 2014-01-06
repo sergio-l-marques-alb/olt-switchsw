a7e5c1321368e3f8e12199ac1fbb039ec0bcbf3c
milton-r-silva <milton-r-silva@alticelabs.com>
2014-01-06 17:45:27 +0000

    Copied files from e500mc processor for helixarm.
    svn path=/branches/helix4/; revision=1209

---
+++ src/fastpath/output/FastPath-Ent-esw-xgs4-helixarm-LR-CSxw-IQH_OLT7-8CH_B/ptin_platform.h
@@ -0,0 +1,8 @@
+/* PTin created
+ * This file defines the platform (switch)
+ *
+ * See l7_platformspecs.h
+ */
+
+#define PLAT_BCM_CHIP   L7_BCM_VALKYRIE2
+#define PTIN_BOARD      PTIN_BOARD_OLT7_8CH_B
