7eb830be7d98941d5c5bca6355c8bc44d03a292f
milton-r-silva <milton-r-silva@alticelabs.com>
2014-04-04 17:53:34 +0000

    Req OLTSWITCH-278 Max frame of 9600 bytes supported.
    svn path=/branches/fp_sdk_6_3_4/; revision=1595

--- src/fastpath/src/l7public/common/ptin/ptin_globaldefs.h
+++ src/fastpath/src/l7public/common/ptin/ptin_globaldefs.h
@@ -37,7 +37,7 @@
 #define PTIN_SYSTEM_MAX_N_FULLSLOTS     18
 #define PTIN_SYSTEM_MAX_N_PORTS         64
 #define PTIN_SYSTEM_MAX_N_LAGS          PTIN_SYSTEM_MAX_N_PORTS
-#define PTIN_SYSTEM_MTU_SIZE            2048
+#define PTIN_SYSTEM_MTU_SIZE            9600
 #define PTIN_SYSTEM_N_EXTENDED_EVCS   (1<<16) /* 65536 */
 
 #define PTIN_IS_PORT_PON(p)           ((((unsigned long long)1 << p) & PTIN_SYSTEM_PON_PORTS_MASK) != 0)
