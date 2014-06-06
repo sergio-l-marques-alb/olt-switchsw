648bfcdd88b05fcd8baef6c51ad2527e3cfff407
marcio-d-melo <marcio-d-melo@alticelabs.com>
2014-06-06 18:42:00 +0000

    Fix OLTSWITCH-358/OLTTS-5474 A thread is blocking the MGMD
    -When board is inserted, board_id is '0' and the force link up process was not triggered
    
    svn path=/trunk/; revision=1800

--- src/fastpath/src/application/ptin/base/ptin_control.c
+++ src/fastpath/src/application/ptin/base/ptin_control.c
@@ -943,6 +943,8 @@ void ptin_control_switchover_monitor(void)
     return;
   }
 
+  osapiSemaTake(ptin_boardaction_sem, L7_WAIT_FOREVER);
+
   /* Update list of active interfaces */
   for (port=0; port<ports_info.number_of_ports; port++)
   {
@@ -954,7 +956,16 @@ void ptin_control_switchover_monitor(void)
         interfaces_active[port] = 1;
       }
     }
+    /* Save board_id */
+    ptin_intf_boardid_get(port, &board_id);
+    if ( (ports_info.port[port].board_id != 0) && (board_id != ports_info.port[port].board_id) )
+    {
+      ptin_intf_boardid_set(port, ports_info.port[port].board_id);
+      LOG_INFO(LOG_CTX_PTIN_CONTROL, "Board id %u set for port %u", ports_info.port[port].board_id, port);
+    }
   }
+
+  osapiSemaGive(ptin_boardaction_sem);
 
   /* Update port settings */
   if (linkscan_update_control)
