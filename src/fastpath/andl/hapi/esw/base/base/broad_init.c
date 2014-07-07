79c44e87cffd6708e8759ebada9cc07f16db7f4c
milton-r-silva <milton-r-silva@alticelabs.com>
2014-07-07 16:15:47 +0000

    Fix OLTSWITCH-387 Inband traffic gets hanged, when related packets rate are excessive.
    svn path=/branches/dev_2.9/; revision=2026

--- src/fastpath/andl/hapi/esw/base/base/broad_init.c
+++ src/fastpath/andl/hapi/esw/base/base/broad_init.c
@@ -2572,8 +2572,16 @@ void hapiBroadFfpSysMacInstall (DAPI_t      *dapi_g,
   /* PTin modified: inband -> Only consider bytes identifying PTIn related packets */
   L7_uchar8       exact_match[] = {FIELD_MASK_NONE, FIELD_MASK_NONE, FIELD_MASK_NONE,
                                    FIELD_MASK_ALL,  FIELD_MASK_ALL,  FIELD_MASK_ALL };
-
   hapiSystemPtr = (BROAD_SYSTEM_t *)dapi_g->system->hapiSystem;
+
+  /* PTin added: inband */
+  BROAD_METER_ENTRY_t meterInfo;
+  meterInfo.cir       = RATE_LIMIT_INBAND;
+  meterInfo.cbs       = 128;
+  meterInfo.pir       = RATE_LIMIT_INBAND;
+  meterInfo.pbs       = 128;
+  meterInfo.colorMode = BROAD_METER_COLOR_BLIND;
+  /* PTin end */
 
   LOG_INFO(LOG_CTX_MISC,"Going to configure Inband Trap rule...");
 
@@ -2605,6 +2613,7 @@ void hapiBroadFfpSysMacInstall (DAPI_t      *dapi_g,
 
     /* PTin added: inband */
     hapiBroadPolicyRuleActionAdd(ruleId, BROAD_ACTION_TRAP_TO_CPU, 0, 0, 0);
+    hapiBroadPolicyRuleMeterAdd(ruleId, &meterInfo);
     /* PTin end */
 
 #ifdef L7_STACKING_PACKAGE
