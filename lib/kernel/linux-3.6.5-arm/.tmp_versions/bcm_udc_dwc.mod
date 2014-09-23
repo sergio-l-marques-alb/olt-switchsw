965f4d695bcb62760218f79bfd627c03ac0b515d
milton-r-silva <milton-r-silva@alticelabs.com>
2014-09-23 20:26:14 +0000

    Added remaining files for linux-3.6.5-arm kernel
    svn path=/trunk/; revision=2328

---
+++ lib/kernel/linux-3.6.5-arm/.tmp_versions/bcm_udc_dwc.mod
@@ -0,0 +1,2 @@
+arch/arm/plat-iproc/../../../../../bcmdrivers/usbd/bcm_udc_dwc.ko
+arch/arm/plat-iproc/../../../../../bcmdrivers/usbd/bcm_udc_dwc.o
---
+++ lib/kernel/linux-3.6.5-arm/lib/zlib_inflate/.built-in.o.cmd
@@ -0,0 +1,1 @@
+cmd_lib/zlib_inflate/built-in.o :=  /home/devtools/dev-bcm-ldk/3.4.7-RC4/iproc/buildroot/output/host/usr/bin/arm-linux-ld -EL    -r -o lib/zlib_inflate/built-in.o lib/zlib_inflate/zlib_inflate.o 
