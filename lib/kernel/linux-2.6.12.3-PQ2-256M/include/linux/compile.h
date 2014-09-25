1b44ce821e2856f6ee89ff3fcbfb2bf6b57c2ca4
milton-r-silva <milton-r-silva@alticelabs.com>
2014-09-25 10:46:29 +0000

    Added generated include files from compilation
    svn path=/trunk/; revision=2365

---
+++ lib/kernel/linux-2.6.12.3-PQ2-256M/include/linux/compile.h
@@ -0,0 +1,8 @@
+/* This file is auto generated, version 4 */
+#define UTS_MACHINE "ppc"
+#define UTS_VERSION "#4 Tue Sep 23 20:40:31 WEST 2014"
+#define LINUX_COMPILE_TIME "20:40:31"
+#define LINUX_COMPILE_BY "olt"
+#define LINUX_COMPILE_HOST "vxcomp-ubuntu"
+#define LINUX_COMPILE_DOMAIN "ptin.corppt.com"
+#define LINUX_COMPILER "gcc version 4.0.2 20060628 (Wasabi)"
