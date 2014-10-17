dfb559c1a848778fbda64274e2dc585a2cbacbd1
milton-r-silva <milton-r-silva@alticelabs.com>
2014-10-17 15:15:50 +0000

    Fix OLTSWITCH-491 MAC-Bridge flows are limited to 512 flows: increased to 4K flows.
    svn path=/branches/2.8/; revision=2478

--- src/fastpath/src/l7public/common/ptin/ptin_globaldefs_ta48ge.h
+++ src/fastpath/src/l7public/common/ptin/ptin_globaldefs_ta48ge.h
@@ -28,7 +28,7 @@
 # define PTIN_SYSTEM_PORTS_MASK        (PTIN_SYSTEM_PON_PORTS_MASK | PTIN_SYSTEM_ETH_PORTS_MASK | PTIN_SYSTEM_10G_PORTS_MASK)
 
 # define PTIN_SYSTEM_N_EVCS            512    /* Maximum nr of EVCs allowed in this equipment */
-# define PTIN_SYSTEM_N_CLIENTS         512    /* 1 client per interface */
+# define PTIN_SYSTEM_N_CLIENTS         4096   /* 1 client per interface */
 # define PTIN_SYSTEM_N_FLOWS_MAX       8192  /* Maximum nr of flows (total) */
 
 # define PTIN_SYSTEM_MAX_BW_POLICERS   1024  /* Maximum number of BW policer */
