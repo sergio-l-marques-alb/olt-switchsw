b54d7d21557356be85dbe8b1315fcc343236008c
milton-r-silva <milton-r-silva@alticelabs.com>
2014-12-02 18:48:16 +0000

    Fix OLTSWITCH-520 ERPS Convergence causes more than 50ms traffic disruption  - Removed printfs.
    svn path=/branches/2.9/; revision=2705

--- src/fastpath/src/application/ptin/base/ptin_hal_erps.c
+++ src/fastpath/src/application/ptin/base/ptin_hal_erps.c
@@ -471,8 +471,6 @@ L7_RC_t ptin_hal_erps_entry_init(L7_uint8 erps_idx)
 
   if (tbl_halErps[erps_idx].used == L7_TRUE) {
     osapiSemaGive(ptin_hal_erps_sem);
-    // Print some Debug
-    ptin_hal_erps_entry_print(erps_idx);
     return L7_SUCCESS;
   }
   
@@ -527,9 +525,6 @@ L7_RC_t ptin_hal_erps_entry_init(L7_uint8 erps_idx)
   osapiSemaGive(ptin_hal_erps_sem);
 
   ptin_hal_erps_countersClear(erps_idx);
-
-  // Print some Debug
-  ptin_hal_erps_entry_print(erps_idx);
 
   return L7_SUCCESS;
 }
