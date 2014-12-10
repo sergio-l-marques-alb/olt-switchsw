dd4c717dc9a92759195ec068aae241dd1bf3a154
milton-r-silva <milton-r-silva@alticelabs.com>
2014-12-10 19:29:26 +0000

    Task OLTSWITCH-577 (merge from trunk, r2728): fp.cli will not auto-shift dhcp options (1607 command).
    svn path=/branches/2.9/; revision=2741

--- src/fastpath.cli/main/main.c
+++ src/fastpath.cli/main/main.c
@@ -4309,7 +4309,7 @@ int main (int argc, char *argv[])
               help_oltBuga();
               exit(0);
             }
-            ptr->flags = (uint32) valued<<8;
+            ptr->flags = (uint32) valued;
           }
 
           // Maximum number of MAC addresses
