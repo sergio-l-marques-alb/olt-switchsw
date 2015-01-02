1a65526ff21bce0da0bc66832b77e0a3c18c8b18
milton-r-silva <milton-r-silva@alticelabs.com>
2015-01-02 17:07:59 +0000

    Fix OLTSWITCH-587 CXO160G: Max number of learnable MACs is limited to 32K. For all SF boards, this limit was increased to 128K.
    svn path=/trunk/; revision=2844

--- src/fastpath/bsp/platform/broadcom/common/include/xgs5.h
+++ src/fastpath/bsp/platform/broadcom/common/include/xgs5.h
@@ -60,7 +60,7 @@
 
 
 /* Max the FDB size for STDL packages only. Performance issues with Stacking */
-#if ( PTIN_BOARD == PTIN_BOARD_CXO640G )          /* PTin added */
+#if ( PTIN_BOARD_IS_MATRIX )          /* PTin added */
  #define PLAT_MAX_FDB_MAC_ENTRIES                  131072
 #else
 #ifdef L7_STACKING_PACKAGE
