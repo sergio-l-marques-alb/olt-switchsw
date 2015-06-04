2b4725e7dd63f95cf7a877b5281e0e34aba6cea3
milton-r-silva <milton-r-silva@alticelabs.com>
2015-06-04 15:31:42 +0000

    Fix OLTSWITCH-708 Added logtraces to detect MII cl22 and cl45 access errors.
    svn path=/trunk/; revision=3304

--- lib/broadcom-sdk-xgs/sdk-xgs-fastpath-6.4.3/broadcom/esw_sdk/src/soc/phy/wcmod/include/wcmod_main.h
+++ lib/broadcom-sdk-xgs/sdk-xgs-fastpath-6.4.3/broadcom/esw_sdk/src/soc/phy/wcmod/include/wcmod_main.h
@@ -157,7 +157,11 @@ typedef int     soc_port_t;
 
 #define CHK_RET_VAL_FUNC(op) \
     { int rv; if ((rv=(op)) != 0) \
-         printf("ERROR %s:%d. #op# returned %d\n", __FILE__,__LINE__,rv);}
+         LOG_ERR(LOG_CTX_SDK, "ERROR %s:%d. #op# returned %d", __FILE__,__LINE__,rv);}
+
+#define CHK_RET_VAL_FUNC_RET(op, rv) \
+    { if ((rv=(op)) != 0) \
+         LOG_ERR(LOG_CTX_SDK, "ERROR %s:%d. #op# returned %d", __FILE__,__LINE__,rv);}
 
 #define WC_AER_BCST_OFS_STRAP   0x1ff
 #define PHYCTRL_MDIO_ADDR_SHARE (1 << 0) /*use phy base addr. to access lanes */
