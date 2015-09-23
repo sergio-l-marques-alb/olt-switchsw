6759438b953c7b36f2146405fc422a8044a313ce
milton-r-silva <milton-r-silva@alticelabs.com>
2015-09-23 14:40:59 +0000

    Task OLTSWITCH-858 Added TT08SXG output platform folder.
    svn path=/branches/fp-sdk-all/; revision=3527

---
+++ src/fastpath/output/FastPath-Ent-dpp-dnx-e500mc-LR-CSxw-IQH_TT08SXG/ptin_platform.h
@@ -0,0 +1,13 @@
+/* PTin created
+ * This file defines the platform (switch)
+ */
+
+#define PLAT_BCM_CHIP   L7_BCM_ARAD
+#define PTIN_BOARD      PTIN_BOARD_TT08SXG
+
+/* SDK version */
+#define SDK_MAJOR_VERSION   6
+#define SDK_MINOR_VERSION   4
+#define SDK_REVISION_ID     4
+#define SDK_PATCH_ID        0
+
