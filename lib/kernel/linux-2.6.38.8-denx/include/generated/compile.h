b0980d99f489f0442af3d3768b9fed0b938b3d5a
joao-v-mateiro <joao-v-mateiro@alticelabs.com>
2015-12-18 12:13:40 +0000

    OLTSWITCH-887 Packet capture capability - Added support for 802.1d (bridge)
    svn path=/trunk/; revision=3765

--- lib/kernel/linux-2.6.38.8-denx/include/generated/compile.h
+++ lib/kernel/linux-2.6.38.8-denx/include/generated/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version 6 */
+/* This file is auto generated, version 8 */
 #define UTS_MACHINE "ppc"
-#define UTS_VERSION "#6 Thu Dec 17 10:34:00 WET 2015"
-#define LINUX_COMPILE_TIME "10:34:00"
+#define UTS_VERSION "#8 Fri Dec 18 12:07:52 WET 2015"
+#define LINUX_COMPILE_TIME "12:07:52"
 #define LINUX_COMPILE_BY "root"
 #define LINUX_COMPILE_HOST "vxcomp-ubuntu"
 #define LINUX_COMPILE_DOMAIN "ptin.corppt.com"
