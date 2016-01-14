858a5f033de6bd0c3e2c0d78c9e4f70b9a9b087d
milton-r-silva <milton-r-silva@alticelabs.com>
2016-01-14 17:05:05 +0000

    Created kernel for OLT1T0 version 3.5/3.6
    svn path=/branches/3.6/; revision=3806

---
+++ lib/kernel/linux-3.6.5-arm-cma/.missing-syscalls.d
@@ -0,0 +1,8 @@
+-:  \
+ /home/olt/svnrepo/olt-switchsw/trunk/lib/kernel/linux-3.6.5-arm-cma/include/linux/kconfig.h \
+ include/generated/autoconf.h \
+ /home/olt/svnrepo/olt-switchsw/trunk/lib/kernel/linux-3.6.5-arm-cma/arch/arm/include/asm/types.h \
+ include/asm-generic/int-ll64.h \
+ arch/arm/include/generated/asm/bitsperlong.h \
+ include/asm-generic/bitsperlong.h \
+ /home/olt/svnrepo/olt-switchsw/trunk/lib/kernel/linux-3.6.5-arm-cma/arch/arm/include/asm/unistd.h
---
+++ lib/kernel/linux-3.6.5-arm-cma/.vmlinux.cmd
@@ -0,0 +1,1 @@
+cmd_vmlinux := /bin/sh scripts/link-vmlinux.sh /opt/broadcom/bin/arm-linux-ld -EL  -p --no-undefined -X --build-id
