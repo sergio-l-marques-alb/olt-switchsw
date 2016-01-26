ccd5de065aac507ee39c30ec801500e2e1c3bf14
sergio-l-marques <sergio-l-marques@alticelabs.com>
2016-01-26 16:21:01 +0000

    svn path=/trunk/; revision=3892

---
+++ lib/kernel/linux-3.8.13/include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 11 */
+/* SMP */
+#define UTS_MACHINE "ppc"
+#define UTS_VERSION "#11 SMP Mon Dec 1 19:23:42 WET 2014"
+#define LINUX_COMPILE_BY "tiago"
+#define LINUX_COMPILE_HOST "vxcomp-ubuntu"
+#define LINUX_COMPILER "gcc version 4.2.2"
