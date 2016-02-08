dc03d8c37fcc1217ecf9a61151700b6babeadb2a
milton-r-silva <milton-r-silva@alticelabs.com>
2016-02-08 16:30:20 +0000

    Task OLTSWITCH-933 OLT7-8CH: Added base folders for switchdrvr.
    svn path=/branches/olt7-8ch/; revision=3952

---
+++ RELEASE_610_FP_6.1.0.5-FastPath-Ent-esw-xgs3-bmw-LR-CSxw-IQH/build.sh
@@ -0,0 +1,3 @@
+cd output/RELEASE_610_FP_6.1.0.5-FastPath-Ent-esw-xgs3-bmw-LR-CSxw-IQH/
+sh build.sh
+cd -
---
+++ RELEASE_610_FP_6.1.0.5-FastPath-Ent-esw-xgs3-bmw-LR-CSxw-IQH/rbuild.sh
@@ -0,0 +1,3 @@
+cd ./output/RELEASE_610_FP_6.1.0.5-FastPath-Ent-esw-xgs3-bmw-LR-CSxw-IQH_OLT7-8CH_A
+sh rbuild.sh $1 $2 $3 $4
+cd -
