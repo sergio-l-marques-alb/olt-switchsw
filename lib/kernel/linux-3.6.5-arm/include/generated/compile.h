bfd3efda39c3c619d63fdc7baca6fa147154294a
milton-r-silva <milton-r-silva@alticelabs.com>
2016-04-06 15:31:11 +0000

    Removed Kernel from fp-sdk-all branch
    svn path=/branches/fp-sdk-all/; revision=4112

--- lib/kernel/linux-3.6.5-arm/include/generated/compile.h
+++
@@ -1,7 +0,0 @@
-/* This file is auto generated, version 5 */
-/* SMP */
-#define UTS_MACHINE "arm"
-#define UTS_VERSION "#5 SMP Wed Dec 31 12:17:06 WET 2014"
-#define LINUX_COMPILE_BY "olt"
-#define LINUX_COMPILE_HOST "vxcomp-ubuntu"
-#define LINUX_COMPILER "gcc version 4.7.2 (Broadcom Linux) "
