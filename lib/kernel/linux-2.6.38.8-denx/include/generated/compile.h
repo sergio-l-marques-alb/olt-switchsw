65663911b70ff05c8697c48ec63192aac306028e
milton-r-silva <milton-r-silva@alticelabs.com>
2016-04-06 15:30:42 +0000

    Removed Kernel from fp-sdk-all branch
    svn path=/branches/fp-sdk-all/; revision=4110

--- lib/kernel/linux-2.6.38.8-denx/include/generated/compile.h
+++
@@ -1,8 +0,0 @@
-/* This file is auto generated, version 6 */
-#define UTS_MACHINE "ppc"
-#define UTS_VERSION "#6 Thu Apr 9 19:01:46 WEST 2015"
-#define LINUX_COMPILE_TIME "19:01:46"
-#define LINUX_COMPILE_BY "olt"
-#define LINUX_COMPILE_HOST "vxcomp-ubuntu"
-#define LINUX_COMPILE_DOMAIN "ptin.corppt.com"
-#define LINUX_COMPILER "gcc version 4.0.2 20060628 (Wasabi)"
