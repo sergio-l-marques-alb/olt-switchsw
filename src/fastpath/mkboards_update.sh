68636a331db2d64578ac5c30f758a174dbf156ff
milton-r-silva <milton-r-silva@alticelabs.com>
2016-04-15 15:19:20 +0000

    Task OLTSWITCH-688 Updated mkboards_update.sh script.
    svn path=/branches/fp-sdk-all/; revision=4138

--- src/fastpath/mkboards_update.sh
+++ src/fastpath/mkboards_update.sh
@@ -13,66 +13,63 @@
 #########################################################################################################
 
 MKBOARDS=$1
-BOARD=$2
 
-if [ $BOARD == "ta12xg" ]; then
- echo "Updating TA12XGE board...$1"
- cd output/FastPath-Ent-dpp-dnx-e500-LR-CSxw-IQH_TA12XG
- cp -v ipl/switchdrvr ipl/devshell_symbols.gz target/*.ko ipl/fp.cli ipl/fp.shell ipl/mgmd.cli $MKBOARDS/TA12XG/rootfs/usr/local/ptin/sbin/
- cp -v ipl/libmgmd.so $MKBOARDS/TA12XG/rootfs/usr/local/ptin/lib/
- cd - > /dev/null 2>&1
- cp -vr ../builds/tmp/TA12XG/rootfs/* $MKBOARDS/TA12XG/rootfs/
- echo "OK!"
+if [ $2 == "ta12xg" ]; then
+ BOARD="TA12XG"
+ OUTPUT=output/FastPath-Ent-dpp-dnx-e500-LR-CSxw-IQH_TA12XG
+elif [ $2 == "tu100g" ]; then
+ BOARD="TU100G"
+ OUTPUT=output/FastPath-Ent-dpp-dnx-e500-LR-CSxw-IQH_TU100G
+elif [ $2 == "tt08sxg" ]; then
+ BOARD="TT08SXG"
+ OUTPUT=output/FastPath-Ent-dpp-dnx-e500mc-LR-CSxw-IQH_TT08SXG
+else
+ echo "Invalid board"
+ exit;
 fi
 
-if [ $BOARD == "tu100g" ]; then
- echo "Updating TU100G board...$1"
- cd output/FastPath-Ent-dpp-dnx-e500-LR-CSxw-IQH_TU100G
- cp -v ipl/switchdrvr ipl/devshell_symbols.gz target/*.ko ipl/fp.cli ipl/fp.shell ipl/mgmd.cli $MKBOARDS/TU100G/rootfs/usr/local/ptin/sbin/
- cp -v ipl/libmgmd.so $MKBOARDS/TU100G/rootfs/usr/local/ptin/lib/
- cd - > /dev/null 2>&1
- cp -vr ../builds/tmp/TU100G/rootfs/* $MKBOARDS/TU100G/rootfs/
- echo "OK!"
-fi
+echo "Clearing output folders..."
+rm -rf $MKBOARDS/$BOARD/rootfs
+mkdir -p $MKBOARDS/$BOARD/rootfs/
+mkdir -p $MKBOARDS/$BOARD/rootfs/usr/local/ptin/sbin/
+mkdir -p $MKBOARDS/$BOARD/rootfs/usr/local/ptin/lib/
+mkdir -p $MKBOARDS/$BOARD/rootfs/usr/local/ptin/scripts/
 
-if [ $BOARD == "tt08sxg" ]; then
- echo "Updating TT08SXG board...$1"
- cd output/FastPath-Ent-dpp-dnx-e500mc-LR-CSxw-IQH_TT08SXG
- cp -v ipl/switchdrvr ipl/devshell_symbols.gz target/*.ko ipl/fp.cli ipl/fp.shell ipl/mgmd.cli $MKBOARDS/TT08SXG/rootfs/usr/local/ptin/sbin/
- cp -v ipl/libmgmd.so $MKBOARDS/TT08SXG/rootfs/usr/local/ptin/lib/
- cd - > /dev/null 2>&1
- cp -vr ../builds/tmp/TT08SXG/rootfs/* $MKBOARDS/TT08SXG/rootfs/
- echo "OK!"
-fi
+echo "Updating $BOARD board..."
+cd $OUTPUT
+cp -v ipl/switchdrvr ipl/devshell_symbols.gz target/*.ko ipl/fp.cli ipl/fp.shell ipl/mgmd.cli $MKBOARDS/$BOARD/rootfs/usr/local/ptin/sbin/
+cp -v ipl/libmgmd.so $MKBOARDS/$BOARD/rootfs/usr/local/ptin/lib/
+cd - > /dev/null 2>&1
+cp -vr ../builds/tmp/$BOARD/rootfs/* $MKBOARDS/$BOARD/rootfs/
+
+version=r`svnversion .. -n | sed -e 's/.*://' -e 's/[A-Z]*$$//'`
+echo echo Modular OLT $MODULE $version > ./fp.ver
+echo echo "Built @ `date`" >> ./fp.ver
+echo "echo OLTSWITCH md5sum: "`md5sum $OUTPUT/ipl/switchdrvr | awk '{print $1}'` >> ./fp.ver
+chmod 777 ./fp.ver
+cp ./fp.ver $MKBOARDS/$BOARD/rootfs/usr/local/ptin/sbin
+echo "OK!"
+
+echo "Backing up unstripped binary..."
+cd $OUTPUT/ipl
+7z a $MKBOARDS/$BOARD/backup/switchdrvr.unstripped_${BOARD}-${version}.7z switchdrvr.unstripped
+cd -
+echo "Done!"
 
 if [ $# -ge 3 ]; then
 	IMAGE_VERSION=$3
 	EQUIP_IP=$4
 	echo -n "Generating image for version $3..."
 
-        if [ $BOARD == "ta12xg" ]; then
-                cd $MKBOARDS/TA12XG/
-                sudo ./build_ramdisk_TA12XG.sh $IMAGE_VERSION > /dev/null 2>&1
-                echo "OK!"
-        fi
+	cd $MKBOARDS/$BOARD/
+	sudo ./build_ramdisk_${BOARD}.sh $IMAGE_VERSION > /dev/null 2>&1
+        echo "OK!"
 
-        if [ $BOARD == "tu100g" ]; then
-                cd $MKBOARDS/TU100G/
-                sudo ./build_ramdisk_TU100G.sh $IMAGE_VERSION > /dev/null 2>&1
-                echo "OK!"
-        fi
-
-        if [ $BOARD == "tt08sxg" ]; then
-                cd $MKBOARDS/TT08SXG/
-                sudo ./build_ramdisk_TT08SXG.sh $IMAGE_VERSION > /dev/null 2>&1
-                echo "OK!"
-        fi
-
-    if [ $# -ge 4 ]; then
-	echo -n "Uploading image to $4..."
-	./upload $EQUIP_IP
-	cd - > /dev/null 2>&1
-	echo "OK!"
-    fi
+	if [ $# -ge 4 ]; then
+		echo -n "Uploading image to $4..."
+		./upload $EQUIP_IP
+		cd - > /dev/null 2>&1
+		echo "OK!"
+	fi
 fi
 
