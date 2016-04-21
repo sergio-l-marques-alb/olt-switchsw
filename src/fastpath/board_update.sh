c0166cda75cc7d87169bcff6d277e49392c06ee4
milton-r-silva <milton-r-silva@alticelabs.com>
2016-04-21 21:38:22 +0000

    Task OLTSWITCH-988 Added script board_update to transfer binaries to a board.
    svn path=/branches/fp-sdk-all/; revision=4148

---
+++ src/fastpath/board_update.sh
@@ -0,0 +1,70 @@
+#!/bin/sh
+
+IP=$2
+DST_PATH=/usr/local/ptin/sbin
+
+USER=root
+PASS=lampada
+
+if [ $1 == "ta12xg" ]; then
+  output=output/FastPath-Ent-dpp-dnx-e500-LR-CSxw-IQH_TA12XG
+elif [ $1 == "tt08sxg" ]; then
+  output=output/FastPath-Ent-dpp-dnx-e500mc-LR-CSxw-IQH_TT08SXG
+elif [ $1 == "tu100g" ]; then
+  output=output/FastPath-Ent-dpp-dnx-e500-LR-CSxw-IQH_TU100G
+else
+  echo "Invalid board"
+  exit;
+fi
+
+if [ $# == 2 ]; then
+  expect -c "
+    spawn telnet $IP
+    expect -re \"login:\"
+    send \"guest\n\"
+    expect -re \"Password:\"
+    send \"$PASS\n\"
+    expect -re \"]$|]#\"
+    send \"su -\n\"
+    expect -re \"Password:\"
+    send \"$PASS\n\"
+    expect -re \"]$|]#\"
+    send \"sh /usr/local/ptin/sbin/fastpath stop\n\"
+    expect -re \"]$|]#\"
+    send \"exit\n\"
+    expect -re \"]$|]#\"
+    send \"exit\n\"
+  "
+elif [ $# == 3 ]; then
+  expect -c "
+    spawn telnet $IP
+    expect -re \"login:\"
+    send \"guest\n\"
+    expect -re \"Password:\"
+    send \"$PASS\n\"
+    expect -re \"]$|]#\"
+    send \"su -\n\"
+    expect -re \"Password:\"
+    send \"$PASS\n\"
+    expect -re \"]$|]#\"
+    send \"telnet 192.168.200.$3\n\"
+    expect -re \"login:\"
+    send \"$USER\n\"
+    expect -re \"Password:\"
+    send \"$PASS\n\"
+    expect -re \"]#\"
+    send \"sh /usr/local/ptin/sbin/fastpath stop\n\"
+    expect -re \"]#\"
+    send \"exit\n\"
+    expect -re \"]#\"
+    send \"exit\n\"
+    expect -re \"]#\"
+    send \"exit\n\"
+  "
+fi
+
+cd $output/ipl
+lc_put $2 $3 switchdrvr $DST_PATH
+lc_put $2 $3 devshell_symbols.gz $DST_PATH
+lc_put $2 $3 fp.cli $DST_PATH
+cd -
--- src/fastpath/mkboards_update.sh
+++ src/fastpath/mkboards_update.sh
@@ -1,27 +1,30 @@
 #!/bin/sh
 
 #########################################################################################################
-#  																																	  #
-# Use this script to updated the desired mkboards folder for the requested card. 							  #
+#  													#
+# Use this script to updated the desired mkboards folder for the requested card. 			#
 # Optionally, you can also generate an image for the selected card and upload it to a remote equipment. #
-#  																																	  #
-# Usage: 																															  #
-# 		./mkboards_update.sh <$MKBOARDS_PATH> <$CARD> [$VERSION $EQUIP_IP]  										  #
-#  																																	  #
-# Note: This script requires permission elevation. 																	  #
-#  																																	  #
+#  													#
+# Usage: 												#
+# 		./mkboards_update.sh <$MKBOARDS_PATH> <$CARD> [$VERSION $EQUIP_IP]  			#
+#  													#
+# Note: This script requires permission elevation. 							#
+#  													#
 #########################################################################################################
 
 MKBOARDS=$1
 
 if [ $2 == "ta12xg" ]; then
  BOARD="TA12XG"
+ MKBOARDS_FOLDER=$BOARD
  OUTPUT=output/FastPath-Ent-dpp-dnx-e500-LR-CSxw-IQH_TA12XG
 elif [ $2 == "tu100g" ]; then
  BOARD="TU100G"
+ MKBOARDS_FOLDER=$BOARD
  OUTPUT=output/FastPath-Ent-dpp-dnx-e500-LR-CSxw-IQH_TU100G
 elif [ $2 == "tt08sxg" ]; then
  BOARD="TT08SXG"
+ MKBOARDS_FOLDER=$BOARD
  OUTPUT=output/FastPath-Ent-dpp-dnx-e500mc-LR-CSxw-IQH_TT08SXG
 else
  echo "Invalid board"
@@ -29,30 +32,31 @@ else
 fi
 
 echo "Clearing output folders..."
-rm -rf $MKBOARDS/$BOARD/rootfs
-mkdir -p $MKBOARDS/$BOARD/rootfs/
-mkdir -p $MKBOARDS/$BOARD/rootfs/usr/local/ptin/sbin/
-mkdir -p $MKBOARDS/$BOARD/rootfs/usr/local/ptin/lib/
-mkdir -p $MKBOARDS/$BOARD/rootfs/usr/local/ptin/scripts/
+rm -rf $MKBOARDS/$MKBOARDS_FOLDER/rootfs
+mkdir -p $MKBOARDS/$MKBOARDS_FOLDER/rootfs/
+mkdir -p $MKBOARDS/$MKBOARDS_FOLDER/rootfs/usr/local/ptin/sbin/
+mkdir -p $MKBOARDS/$MKBOARDS_FOLDER/rootfs/usr/local/ptin/lib/
+mkdir -p $MKBOARDS/$MKBOARDS_FOLDER/rootfs/usr/local/ptin/scripts/
+mkdir -p $MKBOARDS/$MKBOARDS_FOLDER/backup/
 
 echo "Updating $BOARD board..."
 cd $OUTPUT
-cp -v ipl/switchdrvr ipl/devshell_symbols.gz target/*.ko ipl/fp.cli ipl/fp.shell ipl/mgmd.cli $MKBOARDS/$BOARD/rootfs/usr/local/ptin/sbin/
-cp -v ipl/libmgmd.so $MKBOARDS/$BOARD/rootfs/usr/local/ptin/lib/
+cp -v ipl/switchdrvr ipl/devshell_symbols.gz target/*.ko ipl/fp.cli ipl/fp.shell ipl/mgmd.cli $MKBOARDS/$MKBOARDS_FOLDER/rootfs/usr/local/ptin/sbin/
+cp -v ipl/libmgmd.so $MKBOARDS/$MKBOARDS_FOLDER/rootfs/usr/local/ptin/lib/
 cd - > /dev/null 2>&1
-cp -vr ../builds/tmp/$BOARD/rootfs/* $MKBOARDS/$BOARD/rootfs/
+cp -vr ../builds/tmp/$MKBOARDS_FOLDER/rootfs/* $MKBOARDS/$MKBOARDS_FOLDER/rootfs/
 
 version=r`svnversion .. -n | sed -e 's/.*://' -e 's/[A-Z]*$$//'`
 echo echo Modular OLT $MODULE $version > ./fp.ver
 echo echo "Built @ `date`" >> ./fp.ver
 echo "echo OLTSWITCH md5sum: "`md5sum $OUTPUT/ipl/switchdrvr | awk '{print $1}'` >> ./fp.ver
 chmod 777 ./fp.ver
-cp ./fp.ver $MKBOARDS/$BOARD/rootfs/usr/local/ptin/sbin
+cp ./fp.ver $MKBOARDS/$MKBOARDS_FOLDER/rootfs/usr/local/ptin/sbin
 echo "OK!"
 
 echo "Backing up unstripped binary..."
 cd $OUTPUT/ipl
-7z a $MKBOARDS/$BOARD/backup/switchdrvr.unstripped_${BOARD}-${version}.7z switchdrvr.unstripped
+7z a $MKBOARDS/$MKBOARDS_FOLDER/backup/switchdrvr.unstripped_${BOARD}-${version}.7z switchdrvr.unstripped
 cd -
 echo "Done!"
 
@@ -61,8 +65,8 @@ if [ $# -ge 3 ]; then
 	EQUIP_IP=$4
 	echo -n "Generating image for version $3..."
 
-	cd $MKBOARDS/$BOARD/
-	sudo ./build_ramdisk_${BOARD}.sh $IMAGE_VERSION > /dev/null 2>&1
+	cd $MKBOARDS/$MKBOARDS_FOLDER/
+	sudo ./build_ramdisk_${MKBOARDS_FOLDER}.sh $IMAGE_VERSION > /dev/null 2>&1
         echo "OK!"
 
 	if [ $# -ge 4 ]; then
