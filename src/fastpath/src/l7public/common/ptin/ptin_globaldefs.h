9e6fb20d7991c8de959e1b443964f4ba33d349c6
milton-r-silva <milton-r-silva@alticelabs.com>
2016-07-01 17:54:00 +0000

    Task OLTSWITCH-1024 Supported board OLT1T0F within switchdrvr.
    svn path=/branches/dev_3.9_katana2/; revision=4221

--- src/fastpath/src/l7public/common/ptin/ptin_globaldefs.h
+++ src/fastpath/src/l7public/common/ptin/ptin_globaldefs.h
@@ -99,6 +99,10 @@ extern int ptin_sys_number_of_ports;
 #if (PTIN_BOARD == PTIN_BOARD_OLT1T0)
 #include "ptin_globaldefs_olt1t0.h"
 
+/* OLT10F */
+#elif (PTIN_BOARD == PTIN_BOARD_OLT1T0F)
+#include "ptin_globaldefs_olt1t0f.h"
+
 /* OLT7-8CH */
 #elif (PTIN_BOARD == PTIN_BOARD_OLT7_8CH_B)
 #include "ptin_globaldefs_olt7-8ch-b.h"
