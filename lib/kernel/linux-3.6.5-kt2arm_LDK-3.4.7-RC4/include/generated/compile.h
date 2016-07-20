84ff41185d23ea2d1a5ea040ac7710d21194d17c
milton-r-silva <milton-r-silva@alticelabs.com>
2016-07-20 22:20:24 +0000

    Task OLTSWITCH-1026 Added kernel source files for Katana2 based boards.
    svn path=/trunk/; revision=4275

---
+++ lib/kernel/linux-3.6.5-kt2arm_LDK-3.4.7-RC4/include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 178 */
+/* SMP */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#178 SMP Wed Jul 20 21:49:14 WEST 2016"
+#define LINUX_COMPILE_BY "mruas"
+#define LINUX_COMPILE_HOST "mruas-HP-ProBook-650-G1"
+#define LINUX_COMPILER "gcc version 4.7.2 (Buildroot 2012.11) "
