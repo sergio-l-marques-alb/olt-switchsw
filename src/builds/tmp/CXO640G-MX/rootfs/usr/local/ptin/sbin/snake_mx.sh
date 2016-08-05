ac3eed9e7a39dc1a958146033b4a17d086ddef53
milton-r-silva <milton-r-silva@alticelabs.com>
2016-08-05 16:49:32 +0000

    Task OLTSWITCH-1026 Modified snake_mx.sh script to allow multiple linecards testing.
    svn path=/branches/dev_3.9_katana2/; revision=4325

--- src/builds/tmp/CXO640G-MX/rootfs/usr/local/ptin/sbin/snake_mx.sh
+++ src/builds/tmp/CXO640G-MX/rootfs/usr/local/ptin/sbin/snake_mx.sh
@@ -1,31 +1,47 @@
+vlan=100
+evc_base=100
+
 if [ $# -lt 3 ]; then
-  echo "Syntax: $0 <linecard_base_port> <uplink_port1> <uplink_port2>"
+  echo "Syntax: $0 <uplink_port1> <uplink_port2> <lc_base_port> [lc_slots]"
   exit 0;
 fi
 
-lc_base_port=$1
-uplink_port1=$2
-uplink_port2=$3
+uplink_port1=$1
+uplink_port2=$2
+lc_base_port=$3
+lc_slots=1
+if [ $# -ge 4 ]; then
+  lc_slots=$4
+fi
 
-vlan=100
+i=$lc_base_port
+let "last_port=lc_base_port+(4*lc_slots)-1"
 
-let "lc_port1=lc_base_port+0"
-let "lc_port2=lc_base_port+1"
-let "lc_port3=lc_base_port+2"
-let "lc_port4=lc_base_port+3"
+while [ $i -le $last_port ]; do
+  echo "Configuring port $i as promiscuous..."
+  /usr/local/ptin/sbin/fp.cli m 1016 intf=0/$i etype=0
+  let "i=i+1"
+done
 
-/usr/local/ptin/sbin/fp.cli m 1016 intf=0/$lc_port1 etype=0
-/usr/local/ptin/sbin/fp.cli m 1016 intf=0/$lc_port2 etype=0
-/usr/local/ptin/sbin/fp.cli m 1016 intf=0/$lc_port3 etype=0
-/usr/local/ptin/sbin/fp.cli m 1016 intf=0/$lc_port4 etype=0
+evc=$evc_base
+i=$lc_base_port
+echo "Creating EVC $evc..."
+/usr/local/ptin/sbin/fp.cli m 1602 $evc
+/usr/local/ptin/sbin/fp.cli m 1601 $evc 0 0 0 0h 2 0/$uplink_port1/0/$vlan 0/$i/1/$vlan
 
-/usr/local/ptin/sbin/fp.cli m 1602 101
-/usr/local/ptin/sbin/fp.cli m 1602 102
-/usr/local/ptin/sbin/fp.cli m 1602 103
+let "evc=evc+1"
+let "i=i+1"
+while [ $i -lt $last_port ]; do
+  let "i_next=i+1"
+  echo "Creating EVC $evc..."
+  /usr/local/ptin/sbin/fp.cli m 1602 $evc
+  /usr/local/ptin/sbin/fp.cli m 1601 $evc 0 0 0 0h 2 0/$i/0/$vlan 0/$i_next/1/$vlan
+  let "i=i+2"
+  let "evc=evc+1"
+done
 
-/usr/local/ptin/sbin/fp.cli m 1601 101 0 0 0 0h 2 0/$uplink_port1/0/$vlan 0/$lc_port1/1/$vlan
-/usr/local/ptin/sbin/fp.cli m 1601 102 0 0 0 0h 2 0/$lc_port2/0/$vlan 0/$lc_port3/1/$vlan
-/usr/local/ptin/sbin/fp.cli m 1601 103 0 0 0 0h 2 0/$lc_port4/0/$vlan 0/$uplink_port2/1/$vlan
+echo "Creating EVC $evc..."
+/usr/local/ptin/sbin/fp.cli m 1601 $evc 0 0 0 0h 2 0/$last_port/0/$vlan 0/$uplink_port2/0/$vlan
 
 echo "Matrix configured!"
 
