e6c0cda4ce7d38d2732c8e1a0ac7165b658e019d
milton-r-silva <milton-r-silva@alticelabs.com>
2016-09-07 10:30:10 +0000

    Task OLTSWITCH-1050 Applied patch to SDK 6.5.5 to be ran on our platforms.
    svn path=/trunk/; revision=4345

--- lib/bcm.user-sdk-xgs/sdk-all-6.5.5/src/appl/diag/system.c
+++ lib/bcm.user-sdk-xgs/sdk-all-6.5.5/src/appl/diag/system.c
@@ -45,6 +45,10 @@
 
 #include <shared/bsl.h>
 
+#include <unistd.h>
+#include <time.h>
+#include <sys/time.h>
+
 #ifndef __KERNEL__
 #include <sys/types.h>
 #include <netinet/in.h>
@@ -1921,6 +1925,404 @@ diag_rc_load(int unit)
 }
 #endif /* NO_SAL_APPL */
 
+#define LOG_MSG_MAX_MSG_SIZE    512
+#define MAX_OUTBUF_LEN          512 /* Output buffer max length */
+#define MAX_FILE_LEN            15  /* Filename max length */
+#define MAX_FUNC_LEN            35  /* Function max length */
+#define MAX_LINE_LEN            5   /* Line# max length */
+#define MAX_FILEFUNCLINE_LEN    30  /* Filename+function+line# max length */
+#define MAX_TIMESTAMP_LEN       24  /* Timestamp max length*/
+#define MAX_LOG_LINES         4096  /* Max File Lines */
+
+/* Severity */
+typedef enum {
+    LOG_OFF = 0,        /* Turn of any print */
+    LOG_SEV_PRINT,      /* Always print */
+    LOG_SEV_FATAL,
+    LOG_SEV_CRITICAL,
+    LOG_SEV_ERROR,
+    LOG_SEV_WARNING,
+    LOG_SEV_NOTICE,
+    LOG_SEV_INFO,
+    LOG_SEV_DEBUG,
+    LOG_SEV_TRACE,
+    /* Last element */
+    LOG_SEV_LAST,
+} log_severity_t;
+
+/* Severity strings */
+static const char *log_sev_str[LOG_SEV_LAST] = {
+    "OFF",
+    "[PRINT]   ",
+    "[FATAL]   ",
+    "[CRITICAL]",
+    "[ERROR]   ",
+    "[WARNING] ",
+    "[NOTICE]  ",
+    "[INFO]    ",
+    "[DEBUG]   ",
+    "[TRACE]   ",
+};
+
+
+unsigned int bsl_layer_bmp = 0;
+unsigned int bsl_source_bmp[(bslSourceCount/32)+1];
+unsigned char bsl_severity_min = bslSeverityOff;
+
+
+/**
+ * Reset CM LOGGINGs allowed
+ * 
+ * @author mruas (12/30/2014)
+ */
+void hapiBroadCmReset(void)
+{
+  bsl_layer_bmp = 0;
+  memset(bsl_source_bmp, 0, sizeof(bsl_source_bmp));
+  bsl_severity_min = bslSeverityOff;
+}
+
+
+/**
+ * Configure Layers to be logged
+ * 
+ * @author mruas (12/30/2014)
+ * 
+ * @param layer 
+ * @param enable 
+ */
+void hapiBroadCmLayerSet(int layer, int enable)
+{
+  if (layer >= bslLayerCount)
+  {
+    return;
+  }
+
+  if (enable)
+  {
+    bsl_layer_bmp |= (1UL << layer);
+  }
+  else
+  {
+    bsl_layer_bmp &= ~(1UL << layer);
+  }
+}
+
+/**
+ * Configure sources to be logged
+ * 
+ * @author mruas (12/30/2014)
+ * 
+ * @param source 
+ * @param enable 
+ */
+void hapiBroadCmSourceSet(int source, int enable)
+{
+  int div, mod;
+
+  if (source >= bslSourceCount)
+  {
+    return;
+  }
+
+  div = source / (sizeof(uint32)*8);
+  mod = source % (sizeof(uint32)*8);
+
+  if (enable)
+  {
+    bsl_source_bmp[div] |= (1UL << mod);
+  }
+  else
+  {
+    bsl_source_bmp[div] &= ~(1UL << mod);
+  }
+}
+
+/**
+ * Define minimum severity to be logged
+ * 
+ * @author mruas (12/30/2014)
+ * 
+ * @param severity 
+ */
+void hapiBroadCmSeveritySet(int severity)
+{
+  if (severity >= bslSeverityCount)
+  {
+    return;
+  }
+
+  bsl_severity_min = severity;
+}
+
+
+/**
+ * Composes a string with a timestamp
+ * 
+ * @param output Pointer to the output string
+ * 
+ * @return char* Returns the same input pointer
+ */
+static char* get_time(char* output)
+{
+    struct timeval   tv;
+    struct tm        date;
+    time_t           timeabs;
+
+    if (output) {
+        time (&timeabs);
+        localtime_r (&timeabs, &date);
+        gettimeofday (&tv, NULL);
+        sprintf (output, "%04d%02d%02d-%02dh%02dm%02d.%03d",
+                 date.tm_year+1900, date.tm_mon+1, date.tm_mday, date.tm_hour, date.tm_min, date.tm_sec, (int)(tv.tv_usec/1000));
+    }
+
+    return output;
+}
+
+/**
+ * Prints a log message
+ * 
+ * @param ctx  Context
+ * @param sev  Severity
+ * @param file Filename (can be NULL)
+ * @param func Function name (can be NULL)
+ * @param line Line# (if zero, is ignored)
+ * @param fmt  Format string+ arguments (like printf)
+ */
+void logger_print(log_severity_t sev, char const *file,
+                  char const *func, int line, char const *fmt, ...)
+{
+    va_list vargs;
+    char    timestamp[MAX_TIMESTAMP_LEN];
+    char    filefunc[MAX_OUTBUF_LEN];
+    char    outbuf[MAX_OUTBUF_LEN];
+    int     maxlen;
+    int     offset;
+    int     nchars;
+
+    /* Validate input parameters */
+    if ( (sev < 0) || (sev >= LOG_SEV_LAST) ) {
+        return;
+    }
+
+    /* Determine maximum length for file+func+line and compose string */
+    nchars = 0;
+    maxlen = 0;
+    filefunc[0] = '\0';
+    if ( file != NULL ) {
+        maxlen += MAX_FILE_LEN;
+        nchars += snprintf(filefunc+nchars, MAX_OUTBUF_LEN-nchars, "%s ", file);
+    }
+    if ( func != NULL ) {
+        maxlen += MAX_FUNC_LEN;
+        nchars += snprintf(filefunc+nchars, MAX_OUTBUF_LEN-nchars, "%s", func);
+    }
+    if ( line > 0 ) {
+        maxlen += MAX_LINE_LEN;
+        nchars += snprintf(filefunc+nchars, MAX_OUTBUF_LEN-nchars, "(%d)", line);
+    }
+    #if 0
+    else
+    {
+        /* Add the () to the function name (without line nr) */
+        nchars += snprintf(filefunc+nchars, MAX_OUTBUF_LEN-nchars, "()");
+    }
+    #endif
+
+    /* Determine offset to truncate string size */
+    offset = 0;
+    if ( nchars > maxlen ) {
+        offset = nchars - maxlen;
+        filefunc[offset+0] = '.';
+        filefunc[offset+1] = '.';
+        filefunc[offset+2] = '.';
+    }
+
+    /* Print to a string the std pattern */
+    nchars = snprintf(outbuf, MAX_OUTBUF_LEN, "%s %-*s %s ", get_time(timestamp),
+                      maxlen, filefunc+offset, log_sev_str[sev]);
+
+    /* Continue printing the log itself */
+    va_start(vargs, fmt);
+    vsnprintf (outbuf + nchars, MAX_OUTBUF_LEN-nchars, fmt, vargs);
+    va_end(vargs);
+
+    /* Output it... */
+    fprintf(stdout, "%.*s\r\n", MAX_OUTBUF_LEN, outbuf);
+}
+
+
+/**
+ * Callback for checking if logs should be printed
+ * 
+ * @author mruas (12/30/2014)
+ * 
+ * @param meta_pack 
+ * 
+ * @return int 
+ */
+int hapiBroadCmCheck(bsl_packed_meta_t meta_pack)
+{
+    int div, mod;
+    int severity = BSL_SEVERITY_GET(meta_pack);
+    int layer    = BSL_LAYER_GET(meta_pack);
+    int source   = BSL_SOURCE_GET(meta_pack);
+
+    /* Always TRUE */
+    if (severity <= bslSeverityWarn /*|| layer == bslLayerAppl*/) {
+        return 1;
+    }
+
+    /* Check configurable conditions */
+    /* Layer */
+    if ( !(bsl_layer_bmp & (1UL << layer)) )
+    {
+      return 0;
+    }
+    /* Source */
+    div = source/(sizeof(uint32)*8);
+    mod = source%(sizeof(uint32)*8);
+    if ( !(bsl_source_bmp[div] & (1UL << mod)) )
+    {
+      return 0;
+    }
+    /* Severity */
+    if (severity > bsl_severity_min)
+    {
+      return 0;
+    }
+
+    return 1;
+}
+
+
+/**
+ * Callback for LOGs printing
+ * 
+ * @author mruas (12/30/2014)
+ * 
+ * @param meta_data 
+ * @param format 
+ * @param args 
+ * 
+ * @return int 
+ */
+int hapiBroadCmPrint(bsl_meta_t *meta_data, const char *format, va_list args)
+{
+  int logit = 0, printit = 0;
+  char buf[LOG_MSG_MAX_MSG_SIZE];
+  log_severity_t ptin_log_sev = LOG_SEV_PRINT;
+
+  if( (meta_data == NULL) )
+  {
+    /* Always treat no meta data as an immediate print to the console */
+    logit = 0;
+    printit = 1;
+  }
+  else if (meta_data->layer == bslLayerAppl && meta_data->source == bslSourceShell)
+  {
+    /* Always print APPL layer to stdout */
+    logit = 0;
+    printit = 1;
+  }
+  else if (meta_data->severity <= bslSeverityFatal)
+  {
+    logit = 1;
+    printit = 0;
+    ptin_log_sev = LOG_SEV_FATAL;
+  }
+  else if (meta_data->severity <= bslSeverityError)
+  {
+    logit = 1;
+    printit = 0;
+    ptin_log_sev = LOG_SEV_ERROR;
+  }
+  else if (meta_data->severity <= bslSeverityWarn)
+  {
+    logit = 1;
+    printit = 0;
+    ptin_log_sev = LOG_SEV_WARNING;
+  }
+  else if (bsl_check(meta_data->layer, meta_data->source, meta_data->severity, meta_data->unit))
+  {
+    logit = 1;
+    printit = 0;
+
+    if (meta_data->severity <= bslSeverityInfo)
+    {
+      ptin_log_sev = LOG_SEV_INFO;
+    }
+    else
+    {
+      ptin_log_sev = LOG_SEV_DEBUG;
+    }
+  }
+
+  if (printit)
+  {
+    vprintf(format,args);
+  }
+  else if (logit)   /* PTin modified: Logs */
+  {
+    /* 
+     * only allow the write to happen to either syslog or dapiTrace 
+     * in order to reduce time.
+     */
+    int rc = 0;
+
+    rc = vsnprintf(buf, sizeof (buf), format, args);
+  
+    if (rc <= 0)
+    {
+      return 0;  
+    }
+    else
+    {  
+      if ( rc >= sizeof(buf)) 
+      {
+        rc = sizeof(buf) - 1;
+        /* make sure that the string is terminated */
+        buf[rc] =  '\0';
+      }
+
+      /* get rid of new lines */
+      if (buf[rc-1] == '\n') buf[rc-1] = '\0';
+
+      logger_print(ptin_log_sev, meta_data->file, meta_data->func, meta_data->line, "%s", buf);
+    }
+  }
+
+  return 0;
+}
+
+/**
+ * Configure default CM loggings
+ * 
+ * @author mruas (12/30/2014)
+ */
+void hapiBroadCmDefaults(void)
+{
+  hapiBroadCmReset();
+
+  hapiBroadCmLayerSet(bslLayerAppl, 1);
+  hapiBroadCmLayerSet(bslLayerBcm,  1);
+  hapiBroadCmLayerSet(bslLayerBcmx, 1);
+  hapiBroadCmLayerSet(bslLayerSoc,  1);
+  hapiBroadCmLayerSet(bslLayerSys,  1);
+
+  hapiBroadCmSourceSet(bslSourceInit,   1);
+  hapiBroadCmSourceSet(bslSourceShell,  1);
+  hapiBroadCmSourceSet(bslSourceMii,    1);
+  hapiBroadCmSourceSet(bslSourceMiim,   1);
+  hapiBroadCmSourceSet(bslSourceMim,    1);
+  hapiBroadCmSourceSet(bslSourcePhy,    1);
+  hapiBroadCmSourceSet(bslSourcePhymod, 1);
+
+  hapiBroadCmSeveritySet(bslSeverityInfo);
+}
+
+
 /*
  * Diagnostics shell routine.
  */
@@ -1979,6 +2381,18 @@ diag_shell(void)
     parse_user_var_get = diag_user_var_get;
 
     bslmgmt_init();
+
+#if 1
+    bsl_config_t bsl_config;
+
+    bsl_config_t_init(&bsl_config);
+
+    hapiBroadCmDefaults();
+
+    bsl_config.out_hook = hapiBroadCmPrint;
+    bsl_config.check_hook = hapiBroadCmCheck;
+    bsl_init(&bsl_config);
+#endif
 
     diag_init();
 
