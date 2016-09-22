d149190598463a1881adc15708df78061511a149
rui-f-fernandes <rui-f-fernandes@alticelabs.com>
2016-09-22 10:17:42 +0000

    Fix OLTSWITCH-1049/OLTTS-26767 TA48GE - Traffic does not flow when the active Switch Fabric is in slot 1 Reverted isolated port start up in TA48GE board (merge from trunk r4332)
    svn path=/branches/dev_3.10/; revision=4452

--- src/fastpath/src/application/ptin/base/ptin_intf.c
+++ src/fastpath/src/application/ptin/base/ptin_intf.c
@@ -466,7 +466,13 @@ L7_RC_t ptin_intf_portExt_init(void)
   mefExt.doubletag                    = L7_TRUE;
   mefExt.outer_tpid                   = 0x8100;
   mefExt.inner_tpid                   = 0x8100;
+
+  #if (PTIN_BOARD == PTIN_BOARD_TA48GE)
+  mefExt.egress_type                  = PTIN_PORT_EGRESS_TYPE_PROMISCUOUS;
+  #else
   mefExt.egress_type                  = PTIN_PORT_EGRESS_TYPE_ISOLATED;
+  #endif
+
   mefExt.macLearn_enable              = L7_TRUE;
   mefExt.macLearn_stationMove_enable  = L7_TRUE;
   mefExt.macLearn_stationMove_prio    = 0;
