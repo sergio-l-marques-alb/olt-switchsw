28f88013029a363e662ece47d1d1385e93628a85
milton-r-silva <milton-r-silva@alticelabs.com>
2016-11-24 20:15:54 +0000

    Task OLTSWITCH-933 Minor tweak.
    svn path=/trunk/; revision=6040

--- olt7-8ch/run_firsttime.sh
+++ olt7-8ch/run_firsttime.sh
@@ -10,5 +10,6 @@ cd RELEASE_610_FP_6.1.0.5-FastPath-Ent-esw-xgs3-bmw-LR-CSxw-IQH/output/RELEASE_6
 sh correct_snmp_bug.sh
 cd -
 
+echo
 echo "Switchdrvr is ready to be compiled... goto RELEASE_610_FP_6.1.0.5-FastPath-Ent-esw-xgs3-bmw-LR-CSxw-IQH/output/RELEASE_610_FP_6.1.0.5-FastPath-Ent-esw-xgs3-bmw-LR-CSxw-IQH_OLT7-8CH_A folder and type 'sh build.sh'"
 
