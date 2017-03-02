3e7fb0a77153f9f4fc35aef2ec6e0a80d6f24977
rui-f-fernandes <rui-f-fernandes@alticelabs.com>
2017-03-02 10:22:37 +0000

    Req OLTTS-25657/OLTSWITCH-1036 Support NGPON2 logical interfaces in Network Services (merge from branch 3.11 r4342-4344) Port NGPON2 added when adding a group NGPON2. Wrong commit
    svn path=/trunk/; revision=6223

--- src/fastpath/src/application/ptin/base/ptin_evc.c
+++ src/fastpath/src/application/ptin/base/ptin_evc.c
@@ -2572,8 +2572,6 @@ L7_RC_t ptin_evc_create(ptin_HwEthMef10Evc_t *evcConf)
   /* Expand interface formats */
   for (i=0; i<evcConf->n_intf; i++)
   {
-    (evcConf->intf[i].intf.port_type == 4)
-
     /* Convert interface to all formats */
     if (ptin_intf_any_format(&evcConf->intf[i].intf) != L7_SUCCESS)
     {
@@ -5148,16 +5146,10 @@ L7_RC_t ptin_evc_flow_add(ptin_HwEthEvcFlow_t *evcFlow)
     return L7_FAILURE;
   }
 
-  /* Determine leaf ptin_intf */
-  if (evcFlow->ptin_intf.intf_type == PTIN_EVC_INTF_NGPON2)
-   arry_port = [evcFlow->ptin_intf.intf_id];
+  if (evcFlow->ptin_intf.intf_type == PTIN_EVC_INTF_PHYSICAL)
+    leaf_port = evcFlow->ptin_intf.intf_id;
   else
-  {
-    if (evcFlow->ptin_intf.intf_type == PTIN_EVC_INTF_PHYSICAL)
-      leaf_port = evcFlow->ptin_intf.intf_id;
-    else
-      leaf_port = evcFlow->ptin_intf.intf_id + PTIN_SYSTEM_N_PORTS;
-  }
+    leaf_port = evcFlow->ptin_intf.intf_id + PTIN_SYSTEM_N_PORTS;
 
   /* Validate leaf interface (from received message) */
   if ((leaf_port >= PTIN_SYSTEM_N_INTERF) ||
@@ -5231,6 +5223,7 @@ L7_RC_t ptin_evc_flow_add(ptin_HwEthEvcFlow_t *evcFlow)
       e.vport_id  = vport_id & 0xffffff;
       e.pon       = evcFlow->ptin_intf;
       e.gem_id    = evcFlow->uni_ovid;
+      //e.onu       = evcFlow->onuId;
       intf_vp_DB(1, &e);
     }
 
