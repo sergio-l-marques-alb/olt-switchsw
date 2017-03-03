65dfb16be05fda49422b68fa79149a21557a8f4f
milton-r-silva <milton-r-silva@alticelabs.com>
2017-03-03 18:36:30 +0000

    Task OLTSWITCH-1100 Inband traffic bandwidth increased to 4Mbps (download).
    svn path=/trunk/; revision=6250

--- src/fastpath/src/l7public/common/ptin/ptin_globaldefs.h
+++ src/fastpath/src/l7public/common/ptin/ptin_globaldefs.h
@@ -208,7 +208,10 @@ extern int ptin_sys_number_of_ports;
 #define RATE_LIMIT_BCAST    1024    /* 1 Mbps by default */
 #define RATE_LIMIT_MCAST    1024    /* 1 Mbps by default */
 #define RATE_LIMIT_UCUNK    1024    /* 1 Mbps by default */
-#define RATE_LIMIT_CPU      1024    /* 1 Mbps by default */
+#define RATE_LIMIT_CPU_TRAFFIC 1024 /* 1 Mbps by default */
+#define RATE_LIMIT_CPU_TRAPPED 4096 /* 4 Mbps by default */
+#define BUCKET_SIZE_CPU_TRAFFIC 256   /* Bucket size in kbits */
+#define BUCKET_SIZE_CPU_TRAPPED 1024  /* Bucket size in kbits */
 
 /* CoS assigned to trapped packets */
 #define CPU_TRAPPED_PACKETS_COS_DEFAULT 8     /* For IGMP/DHCP/PPPoE/APS/... */
