75707f23585075e346c15449b4e371dd1c4d1949
milton-r-silva <milton-r-silva@alticelabs.com>
2017-03-20 18:23:46 +0000

    Fix OLTSWITCH-1106 Changes from r6306 reverted.
    svn path=/branches/3.7/; revision=6307

--- lib/broadcom-sdk-xgs/sdk-xgs-fastpath-6.3.7/broadcom/esw_sdk/src/soc/common/reg.c
+++ lib/broadcom-sdk-xgs/sdk-xgs-fastpath-6.3.7/broadcom/esw_sdk/src/soc/common/reg.c
@@ -49,9 +49,6 @@
 #include <sal/core/libc.h>
 #include <sal/core/boot.h>
 
-/* PTin added */
-#include "logger.h"
-
 #include <soc/debug.h>
 #include <soc/cm.h>
 #include <soc/drv.h>
@@ -2888,7 +2885,7 @@ soc_reg_addr(int unit, soc_reg_t reg, int port, int index)
     int               gransh; /* index granularity shift */
     soc_block_types_t regblktype;
     soc_block_t       portblktype;
-    int               phy_port = -1;
+    int               phy_port;
     int               instance_mask = 0;
      
 #if defined(BCM_PETRAB_SUPPORT)
@@ -2943,8 +2940,6 @@ soc_reg_addr(int unit, soc_reg_t reg, int port, int index)
                 pindex = SOC_PORT_BINDEX(unit, phy_port);
             } else {
                 block = pindex = -1; /* multiple non-port block */
-                PT_LOG_CRITIC(LOG_CTX_SDK, "port=%d phy_port=%d block=%d pindex=%d portblktype=%d regblktype=%d REG_FIRST_BLK_TYPE=%d SOC_BLOCK_IN_LIST=%d",
-                              port, phy_port, block, pindex, portblktype, regblktype, REG_FIRST_BLK_TYPE(regblktype), SOC_BLOCK_IN_LIST(regblktype,portblktype)); 
             }
         } else if (port == REG_PORT_ANY) {
             block = pindex = -1;
@@ -2971,16 +2966,12 @@ soc_reg_addr(int unit, soc_reg_t reg, int port, int index)
                     }
                 }
                 if (block < 0) {
-                    PT_LOG_CRITIC(LOG_CTX_SDK, "port=%d phy_port=%d block=%d pindex=%d portblktype=%d regblktype=%d REG_FIRST_BLK_TYPE=%d SOC_BLOCK_IN_LIST=%d",
-                                  port, phy_port, block, pindex, portblktype, regblktype, REG_FIRST_BLK_TYPE(regblktype), SOC_BLOCK_IN_LIST(regblktype,portblktype)); 
                     assert(SOC_REG_ADDR_INVALID_PORT); /* invalid port */
                 }
             }
         } else {
         port &= ~SOC_REG_ADDR_INSTANCE_MASK;
         block = pindex = -1;
-        PT_LOG_CRITIC(LOG_CTX_SDK, "port=%d phy_port=%d block=%d pindex=%d portblktype=%d regblktype=%d REG_FIRST_BLK_TYPE=%d SOC_BLOCK_IN_LIST=%d",
-                      port, phy_port, block, pindex, portblktype, regblktype, REG_FIRST_BLK_TYPE(regblktype), SOC_BLOCK_IN_LIST(regblktype,portblktype)); 
         }
     }
 
@@ -3057,8 +3048,6 @@ soc_reg_addr(int unit, soc_reg_t reg, int port, int index)
                 break;
             default:
                 block = -1; /* unknown non-port block */
-                PT_LOG_CRITIC(LOG_CTX_SDK, "port=%d phy_port=%d block=%d pindex=%d portblktype=%d regblktype=%d REG_FIRST_BLK_TYPE=%d SOC_BLOCK_IN_LIST=%d",
-                              port, phy_port, block, pindex, portblktype, regblktype, REG_FIRST_BLK_TYPE(regblktype), SOC_BLOCK_IN_LIST(regblktype,portblktype)); 
                 break;
             }
         }
@@ -3259,21 +3248,12 @@ soc_reg_addr(int unit, soc_reg_t reg, int port, int index)
                 break;
             default:
                     block = -1; /* unknown non-port block */
-                    PT_LOG_CRITIC(LOG_CTX_SDK, "port=%d phy_port=%d block=%d pindex=%d portblktype=%d regblktype=%d REG_FIRST_BLK_TYPE=%d SOC_BLOCK_IN_LIST=%d",
-                                  port, phy_port, block, pindex, portblktype, regblktype, REG_FIRST_BLK_TYPE(regblktype), SOC_BLOCK_IN_LIST(regblktype,portblktype)); 
                     break;
             }
         }
 #ifdef  BCM_SIRIUS_SUPPORT
     }
 #endif
-
-    /* PTin added: sometimes this application crash here! */
-    if (block < 0)
-    {
-      PT_LOG_CRITIC(LOG_CTX_SDK, "assert(block>=0): port=%d phy_port=%d block=%d pindex=%d portblktype=%d regblktype=%d REG_FIRST_BLK_TYPE=%d SOC_BLOCK_IN_LIST=%d",
-                    port, phy_port, block, pindex, portblktype, regblktype, REG_FIRST_BLK_TYPE(regblktype), SOC_BLOCK_IN_LIST(regblktype,portblktype)); 
-    }
 
     assert(block >= 0); /* block must be valid */
 
