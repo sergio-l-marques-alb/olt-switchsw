86ca1de2a1be3e3628adad51b3c69b7020b4bcc3
milton-r-silva <milton-r-silva@alticelabs.com>
2017-06-21 18:30:56 +0000

    Build script for TT08SXG board adapted for kompilovat server
    svn path=/trunk/; revision=6536

--- lib/kernel/linux-3.0.51-p2040/build_kernel_tt08sxg.sh
+++ lib/kernel/linux-3.0.51-p2040/build_kernel_tt08sxg.sh
@@ -20,6 +20,8 @@ CPPFLAGS=" -m32 -mhard-float  -mcpu=e500mc --sysroot=/opt/fsl/1.2/sysroots/ppce5
 
 export CC AS CPPFLAGS LD  CROSS_COMPILE ARCH RANLIB NM CONFIGURE_FLAGS CFLAGS LDFLAGS CXXFLAGS
 
+mv -v .svn .svn.tmp
+
 if [ "$1" == "distclean" ]; then
   make distclean mrproper
   make cxo160g_defconfig
@@ -29,9 +31,11 @@ fi
 
 make clean
 make menuconfig
-make -Wall -j8 uImage
+make -Wall -j `grep -c '^processor' /proc/cpuinfo` uImage
 make modules
 
 scripts/dtc/dtc -O dtb -o tt08sxg.dtb -b 0 -p 1024 arch/powerpc/boot/dts/p2041rdb.dts
 cp arch/powerpc/boot/uImage tt08sxg.kernel.z
+
+mv -v .svn.tmp .svn
 
