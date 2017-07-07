774083ea09f941ecf8e1cd595e0812dd656951c7
milton-r-silva <milton-r-silva@alticelabs.com>
2017-07-07 11:17:12 +0000

    Task OLTSWITCH-48 Default path for the generated tarball is ./output
    svn path=/trunk/; revision=6591

--- src/builds/transfer.sh
+++ src/builds/transfer.sh
@@ -1,23 +1,27 @@
 #!/bin/bash
 
-if [ $# -lt 2 ]; then
- echo "Syntax: $0 <revision> <src_directory>"
+if [ $# -eq 0 ]; then
+ echo "Syntax: $0 <revision> [src_directory=./output]"
  exit 0;
 fi
 
+src_path=./output
+
 rev=$1
+if [ $# -ge 2 ]; then
+ src_path=$2
+fi
 
 ip_addr=10.112.15.243
 username=olt
 password=olttera
-src_path=$2
 dst_path="~/switchdrvr_builds"
 
 echo "Transferring files to olt@10.112.15.243:$dst_path..."
-lftp -u $username,$password $ip_addr -e "cd $dst_path; mput $src_path/fastpath-olt.*r$rev.*; exit"
+lftp -u $username,$password $ip_addr -e "cd $dst_path; mput ${src_path}/fastpath-olt.*-r${rev}.*; exit"
 
 echo "Removing files..."
-rm $src_path/fastpath-olt.*r$rev.*
+rm ${src_path}/fastpath-olt.*r${rev}.*
 
 echo "Done!"
 echo "Goto ${username}@${ip_addr}:${dst_path} and run image_update.sgh script."
