3ad7586235955e6a08c8bca237b38b4d58601480
milton-r-silva <milton-r-silva@alticelabs.com>
2017-07-07 11:32:19 +0000

    Task OLTSWITCH-48 transfer.sh script is operational.
    svn path=/trunk/; revision=6592

--- src/builds/transfer.sh
+++ src/builds/transfer.sh
@@ -1,27 +1,32 @@
 #!/bin/bash
 
-if [ $# -eq 0 ]; then
- echo "Syntax: $0 <revision> [src_directory=./output]"
- exit 0;
-fi
+echo "Syntax: $0 [revision=current] [src_directory=./output]"
 
-src_path=./output
-
-rev=$1
-if [ $# -ge 2 ]; then
- src_path=$2
-fi
-
+# Destination
 ip_addr=10.112.15.243
 username=olt
 password=olttera
 dst_path="~/switchdrvr_builds"
 
+# Default inputs
+svn_rev=`svnversion .. -n | sed -e 's/.*://' -e 's/[A-Z]*$$//'`
+src_path=./output
+
+# Inputs
+if [ $# -ge 1 ]; then
+ svn_rev=$1
+fi
+if [ $# -ge 2 ]; then
+ src_path=$2
+fi
+
+echo "Running '$0 $svn_rev $src_path'..."
+
 echo "Transferring files to olt@10.112.15.243:$dst_path..."
-lftp -u $username,$password $ip_addr -e "cd $dst_path; mput ${src_path}/fastpath-olt.*-r${rev}.*; exit"
+lftp -u $username,$password $ip_addr -e "cd $dst_path; mput ${src_path}/fastpath-olt.*-r${svn_rev}.*; exit"
 
 echo "Removing files..."
-rm ${src_path}/fastpath-olt.*r${rev}.*
+rm ${src_path}/fastpath-olt.*r${svn_rev}.*
 
 echo "Done!"
 echo "Goto ${username}@${ip_addr}:${dst_path} and run image_update.sgh script."
