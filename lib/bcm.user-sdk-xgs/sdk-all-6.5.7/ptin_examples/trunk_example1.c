b0864bc051798f3c2f81fc1413f01dfde6eac77b
milton-r-silva <milton-r-silva@alticelabs.com>
2017-07-11 18:22:38 +0000

    Added to SVN cint scripts to test Trunks with VoQs, and WRED schemes.
    svn path=/trunk/; revision=6607

--- lib/bcm.user-sdk-xgs/sdk-all-6.5.7/ptin_examples/trunk_example1.c
+++ lib/bcm.user-sdk-xgs/sdk-all-6.5.7/ptin_examples/trunk_example1.c
@@ -19,10 +19,10 @@ vswitch_create(4096);
 vswitch_add(4096,0x44801000);
 vswitch_add(4096,0x44801002);
 
-multicast_create(4096);
-vswitch_flood_set(0,4096,16781312,16781312,16781312);
-multicast_vlan_add(16781312,0xc000001,0x44801000);
+multicast_create(4096,1,0);
+/*vswitch_flood_set(0,4096,16781312,16781312,16781312);*/
+multicast_ingress_add(16781312,0xc000001,0x44801000);
 /*multicast_vlan_add(16781312,9,0x44801000);
 multicast_vlan_add(16781312,10,0x44801000);*/
-multicast_vlan_add(16781312,11,0x44801002);
+multicast_ingress_add(16781312,11,0x44801002);
 
