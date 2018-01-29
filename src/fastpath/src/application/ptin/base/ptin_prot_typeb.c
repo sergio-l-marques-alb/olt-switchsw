ef8240cef1aa78f28726ed0d3d57338fa1e08ab2
rui-f-fernandes <rui-f-fernandes@alticelabs.com>
2018-01-29 17:31:12 +0000

    Type-B prot: In v3.7.4-r501 the OLT1T0 when switching to the path of protection DHCP Request (v4 and v6) Protocol doesn't go up. -Revert changes
    svn path=/trunk/; revision=7102

--- src/fastpath/src/application/ptin/base/ptin_prot_typeb.c
+++ src/fastpath/src/application/ptin/base/ptin_prot_typeb.c
@@ -223,8 +223,6 @@ L7_RC_t ptin_prottypeb_intf_switch_notify(L7_uint32 intfNum, L7_uint8 status)
       {
         PT_LOG_TRACE(LOG_CTX_EVC, "Removed Mgmd Port [intIfNum=%u]", intfNum);
       }
-
-    dsBindingClear(intfNum);     
     }
   }
   
