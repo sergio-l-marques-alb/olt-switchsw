f5ca1b474f9ecff8fb82c7256552b5b7ddf1d37c
rui-f-fernandes <rui-f-fernandes@alticelabs.com>
2018-02-16 15:23:25 +0000

    Fix OLTTS-38083 OLT1T0: TYPE-B PROT: DHCPV4: after switch to protection the dhcp lease does not rise up
    svn path=/trunk/; revision=7156

--- src/fastpath/src/application/ptin/base/ptin_prot_typeb.c
+++ src/fastpath/src/application/ptin/base/ptin_prot_typeb.c
@@ -236,10 +236,7 @@ L7_RC_t ptin_prottypeb_intf_switch_notify(L7_uint32 intfNum, L7_uint8 status)
       {
         PT_LOG_TRACE(LOG_CTX_EVC, "Removed Mgmd Port [intIfNum=%u]", intfNum);
       }
-
-#if !PTIN_BOARD_IS_STANDALONE
-      dsBindingClear(intfNum); 
-#endif        
+      dsBindingClear(intfNum);      
     }
   }
   
