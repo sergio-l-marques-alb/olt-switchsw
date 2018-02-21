c6fe040271730a4b33dcbdfe1164f8953f1c9d3b
milton-r-silva <milton-r-silva@alticelabs.com>
2018-02-21 14:53:52 +0000

    Req OLTSWDRV-2 Modified script contents according to the new filesystem organization.
    svn path=/branches/switchdrvr-4.0/; revision=7181

--- src/builds/tmp/tt08sxg/usr/local/scripts/swdrv-scripts/bcm.init.sh
+++ src/builds/tmp/tt08sxg/usr/local/scripts/swdrv-scripts/bcm.init.sh
@@ -1,4 +1,4 @@
 mknod /dev/linux-user-bde c 126 0
 mknod /dev/linux-kernel-bde c 127 0
-insmod linux-kernel-bde.ko dmasize=16M debug=1
-insmod linux-user-bde.ko
+insmod /usr/local/sbin/linux-kernel-bde.ko dmasize=16M debug=1
+insmod /usr/local/sbin/linux-user-bde.ko
