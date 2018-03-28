ece8862080411ee7d48f445c70c8c3bf6f037ed4
rcosta <rcosta@alticelabs.com>
2018-03-28 19:40:40 +0000

    OLTSWDRV-31  Evaluate the Multicast on DNX devices using bcm.user cint examples (When creating LIFs, VSI id should be 0)
    svn path=/branches/switchdrvr-4.0/; revision=7227

--- lib/bcm.user-sdk-xgs/sdk-all-6.5.12/ptin_examples/cint_multicast_example.c
+++ lib/bcm.user-sdk-xgs/sdk-all-6.5.12/ptin_examples/cint_multicast_example.c
@@ -4,7 +4,7 @@
 #include "port.c"
 #include "bridge.c"
 
-int unit=0;
+int unit=0, core=1;
 
 
 global_init(unit);
@@ -25,10 +25,12 @@ bcm_gport_t p[3]={37,38,39};
 
 //bcm_gport_t voqs_gport[3]={0x24000940,0x24000948,0x24000950};   //got these VOQs' gports from "gport" command output corresponding to above ports
 bcm_gport_t voqs_gport[3]={0x24000140,0x24000148,0x24000150};
+BCM_GPORT_UNICAST_QUEUE_GROUP_CORE_QUEUE_SET(voqs_gport[0], core, BCM_GPORT_UNICAST_QUEUE_GROUP_QID_GET(voqs_gport[0]));
+BCM_GPORT_UNICAST_QUEUE_GROUP_CORE_QUEUE_SET(voqs_gport[1], core, BCM_GPORT_UNICAST_QUEUE_GROUP_QID_GET(voqs_gport[1]));
+BCM_GPORT_UNICAST_QUEUE_GROUP_CORE_QUEUE_SET(voqs_gport[2], core, BCM_GPORT_UNICAST_QUEUE_GROUP_QID_GET(voqs_gport[2]));
 
 
-
-int ingress=1;
+int ingress=0;
 int voqs=1;
 int mc_id, vsi;
 
@@ -36,10 +38,11 @@ int mc_id, vsi;
 
 if (ingress) vsi=mc_id=5005;
 else         vsi=mc_id=4096;
-/* Create Multicast group */
-if (ingress)    multicast_create(unit, &mc_id, 1, 0/*Flags*/);
-else            multicast_create(unit, &mc_id, 0 /*Egress*/, 0/*Flags*/);
-vsi=mc_id;
+
+///* Create Multicast group */
+//if (ingress)    multicast_create(unit, &mc_id, 1, 0/*Flags*/);
+//else            multicast_create(unit, &mc_id, 0 /*Egress*/, 0/*Flags*/);
+//vsi=mc_id;
 
 unsigned short vid=100;
 bcm_gport_t vid_prt_gport[3]={0x44801000, 0x44801001, 0x44801002};
@@ -49,6 +52,14 @@ bcm_gport_t vid_prt_gport[3]={0x44801000, 0x44801001, 0x44801002};
 //     BCM_GPORT_VLAN_PORT_ID_SET(vid_prt_gport[i], p[i]<<12 | vid);
 // }
 //} //results in index error
+
+
+
+printf("unit=%d\tcore=%d\tvid=%d\n\r", unit, core, vid);
+printf("ingress=%d \tvoqs=%d \tvsi=%d \tmc_id=%d\n\r", ingress, voqs, vsi, mc_id);
+printf("ports: \t0x%x \t0x%x \t0x%x\n\r", p[0], p[1], p[2]);
+printf("VOQ gports: \t0x%x \t0x%x \t0x%x\n\r", voqs_gport[0], voqs_gport[1], voqs_gport[2]);
+printf("LIF gports: \t0x%x \t0x%x \t0x%x\n\r", vid_prt_gport[0], vid_prt_gport[1], vid_prt_gport[2]);
 
 
 
@@ -63,14 +74,16 @@ vlan_add(unit, vid, p[2]);
 
 /* Create LIFs instantiating the physical ports + VLAN */
 if (voqs) {
-    vp_add(unit, vid_prt_gport[0] /*LIF1 gport*/, 3 /*BCM_VLAN_PORT_MATCH_PORT_VLAN*/, voqs_gport[0] /*Port1*/, vid /*VLAN*/, 0, 0, vsi, 0);
-    vp_add(unit, vid_prt_gport[1] /*LIF2 gport*/, 3 /*BCM_VLAN_PORT_MATCH_PORT_VLAN*/, voqs_gport[1] /*Port2*/, vid /*VLAN*/, 0, 0, vsi, 0);
-    vp_add(unit, vid_prt_gport[2] /*LIF3 gport*/, 3 /*BCM_VLAN_PORT_MATCH_PORT_VLAN*/, voqs_gport[2] /*Port3*/, vid /*VLAN*/, 0, 0, vsi, 0);
+    //Pass "0" instead the target VSI to vp_add, otherwise traffic is looped back. (Perhaps different behaviour if "vswitch_create" already called
+    vp_add(unit, vid_prt_gport[0] /*LIF1 gport*/, 3 /*BCM_VLAN_PORT_MATCH_PORT_VLAN*/, voqs_gport[0] /*Port1*/, vid /*VLAN*/, 0, 0, 0, 0);
+    vp_add(unit, vid_prt_gport[1] /*LIF2 gport*/, 3 /*BCM_VLAN_PORT_MATCH_PORT_VLAN*/, voqs_gport[1] /*Port2*/, vid /*VLAN*/, 0, 0, 0, 0);
+    vp_add(unit, vid_prt_gport[2] /*LIF3 gport*/, 3 /*BCM_VLAN_PORT_MATCH_PORT_VLAN*/, voqs_gport[2] /*Port3*/, vid /*VLAN*/, 0, 0, 0, 0);
 }
 else {
-    vp_add(unit, vid_prt_gport[0] /*LIF1 gport*/, 3 /*BCM_VLAN_PORT_MATCH_PORT_VLAN*/, p[0] /*Port1*/, vid /*VLAN*/, 0, 0, vsi, 0);
-    vp_add(unit, vid_prt_gport[1] /*LIF2 gport*/, 3 /*BCM_VLAN_PORT_MATCH_PORT_VLAN*/, p[1] /*Port2*/, vid /*VLAN*/, 0, 0, vsi, 0);
-    vp_add(unit, vid_prt_gport[2] /*LIF3 gport*/, 3 /*BCM_VLAN_PORT_MATCH_PORT_VLAN*/, p[2] /*Port3*/, vid /*VLAN*/, 0, 0, vsi, 0);
+    //Pass "0" instead the target VSI to vp_add, otherwise traffic is looped back. (Perhaps different behaviour if "vswitch_create" already called
+    vp_add(unit, vid_prt_gport[0] /*LIF1 gport*/, 3 /*BCM_VLAN_PORT_MATCH_PORT_VLAN*/, p[0] /*Port1*/, vid /*VLAN*/, 0, 0, 0, 0);
+    vp_add(unit, vid_prt_gport[1] /*LIF2 gport*/, 3 /*BCM_VLAN_PORT_MATCH_PORT_VLAN*/, p[1] /*Port2*/, vid /*VLAN*/, 0, 0, 0, 0);
+    vp_add(unit, vid_prt_gport[2] /*LIF3 gport*/, 3 /*BCM_VLAN_PORT_MATCH_PORT_VLAN*/, p[2] /*Port3*/, vid /*VLAN*/, 0, 0, 0, 0);
 }
 
 /* Create VSI and attach LIFs */
@@ -82,7 +95,7 @@ vswitch_add(unit, vsi, vid_prt_gport[2] /*LIF3*/);
 
 if (ingress) {
 /* Create Multicast group */
-    //multicast_create(unit, &mc_id, 1, 0/*Flags*/);
+    multicast_create(unit, &mc_id, 1, 0/*Flags*/);
     /* Configure ingress replication */
     if (voqs) {
         multicast_ingress_add(unit, mc_id /*MC group*/, voqs_gport[0], vid_prt_gport[0]);
@@ -97,7 +110,7 @@ if (ingress) {
 }
 else {
 /* Create Multicast group */
-    //multicast_create(unit, &mc_id, 0 /*Egress*/, 0/*Flags*/);
+    multicast_create(unit, &mc_id, 0 /*Egress*/, 0/*Flags*/);
     /* Configure egress replication (MCgroup=16781312=0x1001000) */
     multicast_egress_add(unit, mc_id /*MC group*/, p[0], vid_prt_gport[0]);
     multicast_egress_add(unit, mc_id /*MC group*/, p[1], vid_prt_gport[1]);
