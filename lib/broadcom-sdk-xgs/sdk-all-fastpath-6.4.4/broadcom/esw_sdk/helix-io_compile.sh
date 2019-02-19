9895a8436f3f030e7192278abb04d2e96710e27d
milton-r-silva <milton-r-silva@alticelabs.com>
2019-02-19 11:48:17 +0000

    Removed unncecessary SDKs for AG16GA board.
    svn path=/branches/swdrv-4.x_AG16GA/; revision=7302

--- lib/broadcom-sdk-xgs/sdk-all-fastpath-6.4.4/broadcom/esw_sdk/helix-io_compile.sh
+++
@@ -1,7 +0,0 @@
-CROSS_COMPILER=/opt/broadcom/bin/arm-broadcom-linux-uclibcgnueabi-
-
-KFLAG_INCLD=/opt/broadcom/lib/gcc/arm-broadcom-linux-uclibcgnueabi/4.7.2/include
-
-KFLAGS="-D__LINUX_ARM_ARCH__=7 -D__KERNEL__ -isystem $KFLAG_INCLD -Wall -Wstrict-prototypes -Wno-trigraphs -Os -fno-strict-aliasing -fno-common -marm -mabi=aapcs-linux -fno-pic -pipe -msoft-float -ffreestanding -march=armv7-a -mfpu=vfp -mfloat-abi=softfp -fomit-frame-pointer -g -fno-stack-protector -Wdeclaration-after-statement -Wno-pointer-sign -mlong-calls"
-
-${CROSS_COMPILER}gcc ${KFLAGS} -o helix-io helix-io.c
