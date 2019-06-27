1a2a40d88d849dcb5a0c583020c27a32fcad608c
milton-r-silva <milton-r-silva@alticelabs.com>
2019-06-27 17:14:31 +0000

    Req OLTSWITCH-1255 Added output directory for AE48GE board.
    svn path=/branches/4.1/; revision=7367

---
+++ src/fastpath/output/FastPath-Ent-esw-xgs4-armv7-LR-CSxw-IQH_AE48GE/ptin_platform.h
@@ -0,0 +1,13 @@
+/* PTin created
+ * This file defines the platform (switch)
+ */
+
+#define PLAT_BCM_CHIP   L7_BCM_HURRICANE3MG
+#define PTIN_BOARD      PTIN_BOARD_AE48GE
+
+/* SDK version */
+#define SDK_MAJOR_VERSION   6
+#define SDK_MINOR_VERSION   5
+#define SDK_REVISION_ID     15
+#define SDK_PATCH_ID        0
+
