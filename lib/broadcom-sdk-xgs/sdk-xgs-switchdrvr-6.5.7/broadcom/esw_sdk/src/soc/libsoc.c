c8f888df943ef431b7fc2de8ff51a8768ef8bbf9
rui-f-fernandes <rui-f-fernandes@alticelabs.com>
2020-08-25 08:07:57 +0000

    Fix OLTSWITCH-1347 Release of version 3.14.2 - Create tags 3.14.2
    svn path=/tags/3.14.2/; revision=7703

--- lib/broadcom-sdk-xgs/sdk-xgs-switchdrvr-6.5.7/broadcom/esw_sdk/src/soc/libsoc.c
+++
@@ -1,7 +0,0 @@
-/*
- * $Id: libsoc.c,v 1.3 Broadcom SDK $
- * $Copyright: (c) 2016 Broadcom.
- * Broadcom Proprietary and Confidential. All rights reserved.$
- */
-
-int _libsoc_a_not_empty;
--- lib/kernel/linux-3.6.5-kt2arm_LDK-3.4.7-RC4/bcmdrivers/usb3h/Readme.txt
+++
@@ -1,1 +0,0 @@
-enable USB_XHCI_HCD
