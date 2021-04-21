078c0088f46317311947c535cf1d1d930fd11f10
j-meireis-nogueira <j-meireis-nogueira@alticelabs.com>
2021-04-21 17:22:20 +0000

    Task OLTSWITCH-1466 create a readme file with the basic intructions and local_manager.sh to setup environment and clean local repo folders
    svn path=/trunk/; revision=8251

---
+++ local_manager.sh
@@ -0,0 +1,103 @@
+#!/bin/sh
+
+print_help()
+{
+  echo "Usage: $APP [OPTION] [version]"
+  echo ""
+  echo "Examples:"
+  echo "  $APP -set                              # Setup local repo automatic"
+  echo "  $APP -set trunk                        # Setup local repo as trunk"
+  echo "  $APP -set 3.17                         # Setup local repo as version 3.17"
+  echo "  $APP -clean                            # Clean repo (remove files from compilations)"
+  echo ""
+  echo "version:"
+  echo "  trunk   4.4   4.3 - etc."
+  echo ""
+  echo "Options:"
+  echo "  -s, -set          Setup local repo"
+  echo "  -clean            Clean repo (remove files from compilations)"
+  echo ""
+}
+
+setup_repo()
+{
+    if [ ! -d "../../netband-libs" ]; then
+        echo "$APP: netband-libs do not exists"
+        ln -s /home/olt_shared/switchdrvr/netband-libs ../../netband-libs
+        echo "$APP: netband-libs is created"
+    else
+        echo "$APP: netband-libs already exists"
+    fi
+
+    if [ ! -d "./build_dir" ]; then
+        echo "$APP: build_dir do not exists"
+        if [ "$VERSION" == "trunk" ]; then
+            ln -s /home/olt_shared/oltosng/build_dir
+            echo "$APP: build_dir is created, pointing to -> /home/olt_shared/oltosng/build_dir"
+        else
+            if [ -d "/home/olt_shared/oltosng/build_dir_$VERSION" ]; then
+                ln -s /home/olt_shared/oltosng/build_dir_$VERSION
+                echo "$APP: build_dir is created, pointing to -> /home/olt_shared/oltosng/build_dir_$VERSION"
+            else
+                echo "$APP: ERROR! the build_dir folder for version $VERSION do not exist (/home/olt_shared/oltosng/build_dir_$VERSION)"
+            fi
+        fi
+    else
+        echo "$APP: build_dir already exists"
+    fi
+}
+
+clean_repo()
+{
+  echo "$APP: Clean $LINK_ARCH'"
+  cd src/$LINK_ARCH
+  make cleanall
+  cd ../..
+  rm -rf build_dir_local/*
+}
+
+APP=`basename $0`
+VERSION="trunk"
+LINK_ARCH=$(ls src | grep -m 1 -oP '(swdrv)|(fastpath)')
+
+#Arguments
+arg=`echo "$1" | cut -f1 -d' '`
+case $arg in
+  "-h"|"--h"|"-help"|"--help")
+    print_help
+    exit 0
+    ;;
+  "-s"|"-set")
+    re="([0-9].[0-9]+)|(trunk)"
+    if [[ $2 =~ $re ]]; then
+      VERSION=$2
+    elif [ ! -z "$2" ]; then
+      print_help
+      exit 1
+    else
+      get_version=$(pwd | grep -oP "$re")
+      if [ ! -z "$get_version" ]; then
+        VERSION=$get_version
+      else
+        echo "$APP: Setup repo -> set defaul 'trunk'"
+        VERSION="trunk"
+      fi
+    fi
+    echo "$APP: Setup repo -> $VERSION"
+    setup_repo
+    exit 0
+    ;;
+  "-clean")
+  echo "$APP: Clean repo"
+    clean_repo
+    exit 0
+    ;;
+  *)
+    echo "$APP: invalid option -- '$arg'"
+    print_help
+    exit 1
+    ;;
+esac
+
+print_help
+exit 0
