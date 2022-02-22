b8f7e95952e172dc85358fba4a114f357a81513f
rui-f-fernandes <rui-f-fernandes@alticelabs.com>
2022-02-22 12:15:22 +0000

    Fix OLTSWITCH-1530/OLTTS-45625 [TA48GE] Card in configuration error in version " v3.18.0-r6286" - Interface number variables not coeherent - Log improvement
    svn path=/trunk/; revision=8397

--- src/fastpath/src/system_support/base/infrastructure/nim/nim_util.c
+++ src/fastpath/src/system_support/base/infrastructure/nim/nim_util.c
@@ -1391,7 +1391,9 @@ L7_INTF_STATES_t nimUtilIntfStateGet(L7_uint32 intIfNum)
   }
   else if ((intIfNum < 1) || (intIfNum > platIntfTotalMaxCountGet()))
   {
-    NIM_LOG_MSG("NIM: intIfNum out of range (intUfNum %u)\n", intIfNum);
+    NIM_LOG_MSG("NIM: intIfNum out of range (intUfNum %u max %d)\n", 
+                intIfNum, 
+                platIntfTotalMaxCountGet());
   }
   else
   {
