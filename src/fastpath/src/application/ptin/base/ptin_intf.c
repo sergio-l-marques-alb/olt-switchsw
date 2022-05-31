6a7e8f12954575dc4bf4bb86390703f105025301
rui-f-fernandes <rui-f-fernandes@alticelabs.com>
2022-05-31 11:10:12 +0000

    Fix OLTSWITCH-1405 Support MEO Services on TC16SXG - reduced log
    svn path=/trunk/; revision=8502

--- src/fastpath/src/application/ptin/base/ptin_intf.c
+++ src/fastpath/src/application/ptin/base/ptin_intf.c
@@ -2600,7 +2600,7 @@ L7_RC_t ptin_intf_intIfNum2port(L7_uint32 intIfNum, L7_uint16 virtual_vid,
   if ( _ptin_port >= PTIN_SYSTEM_N_INTERF ||
       (_ptin_port >= ptin_sys_number_of_ports && _ptin_port < PTIN_SYSTEM_N_PORTS))
   {
-    PT_LOG_WARN(LOG_CTX_INTF, "intIfNum# %u is not assigned! (%u)", intIfNum, _ptin_port);
+    PT_LOG_TRACE(LOG_CTX_INTF, "intIfNum# %u is not assigned! (%u)", intIfNum, _ptin_port);
     return L7_FAILURE;
   }
 
