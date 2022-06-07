1ec24f771af1d8fb9d596f3172bb68ececf7d124
rui-f-fernandes <rui-f-fernandes@alticelabs.com>
2022-06-07 10:49:44 +0000

    Fix OLTSWITCH-1564/OLTTS-47880 [TC16SXG] Error "0x08030001" when trying active more than 510 Unicast Client Services - Change xlate hash table parameters to avoid colisions
    svn path=/trunk/; revision=8513

--- src/fastpath/andl/hapi/esw/ptin/ptin_hapi.c
+++ src/fastpath/andl/hapi/esw/ptin/ptin_hapi.c
@@ -409,6 +409,30 @@ L7_RC_t ptin_hapi_hash_init(void)
     rc = L7_FAILURE;
   }
 
+  if (bcm_switch_control_set(0, bcmSwitchHashMultiMoveDepthEgressVlanTranslate1, 2) != BCM_E_NONE)
+  {
+    PT_LOG_ERR(LOG_CTX_HAPI, "Error setting bcmSwitchHashMultiMoveDepthEgressVlanTranslate1 switch_control to 2");
+    rc = L7_FAILURE;
+  }
+
+  if (bcm_switch_control_set(0, bcmSwitchHashMultiMoveDepthEgressVlanTranslate2, 2) != BCM_E_NONE)
+  {
+    PT_LOG_ERR(LOG_CTX_HAPI, "Error setting bcmSwitchHashMultiMoveDepthEgressVlanTranslate2 switch_control to 2");
+    rc = L7_FAILURE;
+  }
+
+  if (bcm_switch_control_set(0, bcmSwitchHashMultiMoveDepthVlanTranslate1, 2) != BCM_E_NONE)
+  {
+    PT_LOG_ERR(LOG_CTX_HAPI, "Error setting bcmSwitchHashMultiMoveDepthVlanTranslate1 switch_control to 2");
+    rc = L7_FAILURE;
+  }
+
+  if (bcm_switch_control_set(0, bcmSwitchHashMultiMoveDepthVlanTranslate2, 2) != BCM_E_NONE)
+  {
+    PT_LOG_ERR(LOG_CTX_HAPI, "Error setting bcmSwitchHashMultiMoveDepthVlanTranslate2 switch_control to 2");
+    rc = L7_FAILURE;
+  }
+
 #if (PTIN_BOARD == PTIN_BOARD_OLT1T0)
   L7_uint32 i, banks;
   L7_int    hash_type;
