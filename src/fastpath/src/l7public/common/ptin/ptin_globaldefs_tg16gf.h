a5398f70e0cbbe2e277158728ecbebb3430c9c51
rui-f-fernandes <rui-f-fernandes@alticelabs.com>
2022-11-16 11:59:50 +0000

    Fix OLTTS-49828/OLTSWITCH-1644 [MAC Limit] Alarm of MAC limit do not show when switch-fabric board 20 is active - The IP of slot 20 SF was sent to the other SF, on the alarms routine of TG16GF
    svn path=/trunk/; revision=8629

--- src/fastpath/src/l7public/common/ptin/ptin_globaldefs_tg16gf.h
+++ src/fastpath/src/l7public/common/ptin/ptin_globaldefs_tg16gf.h
@@ -121,12 +121,7 @@
 # define IPC_LOCALHOST_IPADDR          0x7F000001  /* 127.0.0.1 */
 # define IPC_SERVER_IPADDR_WORKING     0xC0A8C865  /* 192.168.200.101: Working Matrix */
 # define IPC_SERVER_IPADDR_PROTECTION  0xC0A8C866  /* 192.168.200.102: Protection Matrix */
-#ifdef MAP_CPLD
-/* Depends of active matrix */
-# define IPC_SERVER_IPADDR             ((cpld_map->reg.slot_matrix & 1) ? IPC_SERVER_IPADDR_WORKING : IPC_SERVER_IPADDR_PROTECTION)
-#else
-# define IPC_SERVER_IPADDR             0xC0A8C865  /* 192.168.200.101 Hardcoded!!! */
-#endif
+# define IPC_SERVER_IPADDR             ((CPLD_SLOT_MATRIX_GET() & 1) ? IPC_SERVER_IPADDR_WORKING : IPC_SERVER_IPADDR_PROTECTION)
 
 // MX IP address
 # define IPC_MX_IPADDR_WORKING      0xC0A8C801  /* 192.168.200.1: Working Matrix */
