a246d264d852b1fa9c3dd25c7f70e881a974fa92
rui-f-fernandes <rui-f-fernandes@alticelabs.com>
2023-02-24 17:26:43 +0000

    Fix OLTTS-50669 /OLTSWITCH-1659 Traffic does not flow in downstream after removing Bitstream service and creating a Macbridge service - Remove EVC's without leaf ports (except is a P2MP)
    svn path=/trunk/; revision=8665

--- src/fastpath/src/application/ptin/base/ptin_evc.c
+++ src/fastpath/src/application/ptin/base/ptin_evc.c
@@ -3855,10 +3855,12 @@ _ptin_evc_port_remove1:
     return rc;
   }
 
-  /* If there is only 1 port, EVC will be destroyed, except root ports on Linecards.
+  /* If there is only 1 port, EVC will be destroyed, except with is root ports on Linecards
+     of non P2MP (unicast and bitsteam unstacked).
      On SF doens't make sense to have a EVC without root ports */
 #if (PTIN_BOARD_IS_GPON)
-  if ((evcs[evc_idx].n_roots + evcs[evc_idx].n_leafs) < 1)
+  if (((evcs[evc_idx].n_roots + evcs[evc_idx].n_leafs) < 1) || 
+      ((evcs[evc_idx].n_leafs == 0) && !IS_EVC_P2MP(evc_idx)))
 #else
   if ((evcs[evc_idx].n_roots + evcs[evc_idx].n_leafs) <= 1)
 #endif
